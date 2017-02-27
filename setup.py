b712e527cccff68dd8c68dde69524c4770a04d52
Mark Mossberg <mark@trailofbits.com>
2017-02-27 14:59:47 -0500

    Add deps to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='Manticore',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=[
+        'capstone',
+        'pyelftools',
+        'unicorn'
+    ],
     entry_points={
         'console_scripts': [
             'manticore = manticore.__main__:main'
