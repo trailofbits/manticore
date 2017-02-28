8da075b14f55693b1f0f74f31693af06ad67790d
Mark Mossberg <mark@trailofbits.com>
2017-02-28 17:30:02 -0500

    Rm linux.sys_deallocate

--- manticore/models/linux.py
+++ manticore/models/linux.py
@@ -1331,49 +1331,6 @@ class Linux(object):
             raise ProcessExit(error_code)
         return error_code
 
-    def sys_deallocate(self, cpu, addr, size):
-        ''' deallocate - remove allocations
-        The  deallocate  system call deletes the allocations for the specified
-        address range, and causes further references to the addresses within the
-        range to generate invalid memory accesses. The region is also 
-        automatically deallocated when the process is terminated.
-
-        The address addr must be a multiple of the page size.  The length parameter
-        specifies the size of the region to be deallocated in bytes.  All pages 
-        containing a part of the indicated range are deallocated, and subsequent
-        references will terminate the process.  It is not an error if the indicated
-        range does not contain any allocated pages.
-
-        The deallocate function is invoked through system call number 6.
-
-        @param cpu: current CPU
-        @param addr: the starting address to unmap.
-        @param size: the size of the portion to unmap.
-        @return 0        On success
-                EINVAL   addr is not page aligned.
-                EINVAL   length is zero.
-                EINVAL   any  part  of  the  region  being  deallocated  is outside the valid
-                         address range of the process.
-
-        @param cpu: current CPU.
-        @return: C{0} on success.  
-        '''
-        logger.debug("DEALLOCATE(0x%08x, %d)"%(addr, size))
-
-        if addr & 0xfff != 0:
-            logger.info("DEALLOCATE: addr is not page aligned")
-            return errno.EINVAL
-        if size == 0 :
-            logger.info("DEALLOCATE:length is zero")
-            return errno.EINVAL
-        #unlikely AND WRONG!!!
-        #if addr > Decree.CGC_SSIZE_MAX or addr+size > Decree.CGC_SSIZE_MAX:
-        #    logger.info("DEALLOCATE: part of the region being deallocated is outside the valid address range of the process")
-        #    return Decree.CGC_EINVAL
-
-        cpu.memory.munmap(addr, size)
-        return 0
-
     def sys_fdwait(self, cpu, nfds, readfds, writefds, timeout, readyfds):
         ''' fdwait - wait for file descriptors to become ready
         '''
@@ -2107,15 +2064,6 @@ class SLinux(Linux):
             raise SymbolicSyscallArgument(2)
 
         return super(SLinux, self).sys_write(cpu, fd, buf, count)
-
-    def sys_deallocate(self, cpu, addr, size):
-        if issymbolic(addr):
-            logger.debug("Ask to DEALLOCATE a symbolic pointer?!")
-            raise SymbolicSyscallArgument(0)
-        if issymbolic(size):
-            logger.debug("Ask to DEALLOCATE a symbolic size?!")
-            raise SymbolicSyscallArgument(1)
-        return super(SLinux, self).sys_deallocate(cpu, addr, size)
 
 class DecreeEmu(object):
 
