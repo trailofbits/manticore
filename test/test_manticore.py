3faaebdaf324f434cb993497ab15d69757fab00a
Mark Mossberg <mark.mossberg@gmail.com>
2017-03-06 18:39:31 -0500

    Fix stdin/stdout linux file generation, add test (#50)
    * Support linux read/write in stdout/stdin files
    
    * Add basic binary
    
    * Add basic integration test
    
    * Skip
    
    * Update skip message
    
    * Add github issue

--- test/test_manticore.py
+++ test/test_manticore.py
@@ -25,3 +25,20 @@ class ManticoreTest(unittest.TestCase):
             @self.m.hook('0x00400e40')
             def tmp(state):
                 pass
+
+    @unittest.skip('TODO(mark): (#52) activating this test breaks something z3 related for following tests')
+    def test_integration_basic_stdin(self):
+        import os, struct
+        self.m = Manticore('test/binaries/basic_linux_amd64')
+        self.m.run()
+        workspace = os.path.join(os.getcwd(), self.m.workspace)
+        with open(os.path.join(workspace, 'test_00000001.stdin')) as f:
+            a = struct.unpack('<I', f.read())[0]
+        with open(os.path.join(workspace, 'test_00000002.stdin')) as f:
+            b = struct.unpack('<I', f.read())[0]
+        if a > 0x41:
+            self.assertTrue(a > 0x41)
+            self.assertTrue(b <= 0x41)
+        else:
+            self.assertTrue(a <= 0x41)
+            self.assertTrue(b > 0x41)
