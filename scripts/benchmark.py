3c5d64762b737e515c526f39a4b82cb6305c8e1b
JP Smith <jp.smith@trailofbits.com>
2017-03-20 18:28:53 -0400

    add results class, collect more information, and fix check for no args

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,38 +1,84 @@
 from manticore import Manticore
 from sys import argv, exit
-from time import time
 
-def print_results(time_elapsed, instructions_executed):
-    print "  Total time: {} seconds".format(time_elapsed)
-    print "  Total instructions executed: {}".format(instructions_executed)
-    print "  Average instructions per second: {}".format(instructions_executed / time_elapsed)
+import pstats
+
+class Results(object):
+    def __init__(self, time_elapsed, instructions_executed, loading_time, saving_time, solver_time):
+        self.time_elapsed = time_elapsed
+        self.instructions_executed = instructions_executed
+        self.loading_time = loading_time
+        self.saving_time = saving_time
+        self.solver_time = solver_time
+
+    def display(self):
+        print "  Total time: {} seconds".format(self.time_elapsed)
+        print "  Total instructions executed: {}".format(self.instructions_executed)
+        print "  Average instructions per second: {}".format(self.instructions_executed / self.time_elapsed)
+        print "  Time spent loading states: {} seconds".format(self.loading_time)
+        print "  Time spent saving states: {} seconds".format(self.saving_time)
+        print "  Time spent in solver: {} seconds".format(self.solver_time)
 
 def benchmark(program):
     print "[*] Benchmarking program \"{}\"".format(program)
-    time_started = time()
 
     m = Manticore(program)
     m.should_profile = True
     m.run()
 
-    time_elapsed = time() - time_started
     instructions_executed = m._executor.count
 
-    print_results(time_elapsed, instructions_executed)
-    return(time_elapsed, instructions_executed)
+    class PstatsFormatted:
+        def __init__(self, d):
+            self.stats = dict(d)
+        def create_stats(self):
+            pass
+
+    ps = None
+    for item in m._executor._stats:
+            try:
+                stat = PstatsFormatted(item)
+                if ps is None:
+                    ps = pstats.Stats(stat)
+                else:
+                    ps.add(stat)
+            except TypeError:
+                logger.debug("Incorrectly formatted profiling information in _stats, skipping")
+
+    time_elapsed = ps.total_tt
+
+    loading_time = 0
+    saving_time = 0
+    solver_time = 0
+    for (func_file, _, func_name), (_, _, _, func_time, _) in ps.stats.iteritems():
+        if func_name == '_getState':
+            loading_time += func_time
+        elif func_name == '_putState':
+            saving_time += func_time
+        elif func_file.endswith('solver.py') and 'setstate' not in func_name and 'getstate' not in func_name and 'ckl' not in func_name:
+            solver_time += func_time
+
+    results = Results(time_elapsed, instructions_executed, loading_time, saving_time, solver_time)
+
+    results.display()
+    return results
 
 if __name__ == "__main__":
-    if len(argv) == 0:
+    args = argv[1:]
+
+    if len(args) == 0:
         print "usage: python benchmark.py PROGRAM1 PROGRAM2..."
         exit()
 
-    instructions_executed = 0
-    time_elapsed = 0
+    overall_results = Results(0, 0, 0, 0, 0)
 
-    for program in argv[1:]:
-        (seconds, instructions) = benchmark(program)
-        time_elapsed += seconds
-        instructions_executed += instructions
+    for program in args:
+        results = benchmark(program)
+        overall_results.time_elapsed += results.time_elapsed
+        overall_results.instructions_executed += results.instructions_executed
+        overall_results.loading_time += results.loading_time
+        overall_results.saving_time += results.saving_time
+        overall_results.solver_time += results.solver_time
         
     print "Overall:"
-    print_results(time_elapsed, instructions_executed)
+    overall_results.display()
