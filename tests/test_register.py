6ae1523f34891036c7cf0bedc5cff882d887ffbe
Mark Mossberg <mark@trailofbits.com>
2017-03-20 11:52:06 -0400

    Fix register tests
    Changed the interfaces:
    - removed nbits param (never used)
    - no longer raise AssertionError when overflowing a flag reg, just
      truncate
    - rename test funcs to be more descriptive

--- tests/test_register.py
+++ tests/test_register.py
@@ -7,46 +7,31 @@ class RegisterTest(unittest.TestCase):
         r = Register(32)
         self.assertEqual(r.read(), 0)
 
-    def test_rd2(self):
+    def test_basic_write(self):
         r = Register(32)
         r.write(1)
         self.assertEqual(r.read(), 1)
 
-    def test_rd3(self):
-        r = Register(32)
-        r.write(2)
-        self.assertEqual(r.read(1), 0)
-
-    def test_rd4(self):
-        r = Register(32)
-        r.write(7, 2)
-        self.assertEqual(r.read(), 3)
-
-    def test_rd5(self):
+    def test_truncate(self):
         r = Register(32)
         r.write(2**32)
         self.assertEqual(r.read(), 0)
 
-    def test_rd6(self):
+    def test_largest_write(self):
         r = Register(32)
         r.write(0xffffffff)
         self.assertEqual(r.read(), 0xffffffff)
 
-    def test_rd7(self):
-        r = Register(32)
-        r.write(0xffffffff)
-        self.assertEqual(r.read(16), 0xffff)
-
-    def test_reg1(self):
+    def test_flag(self):
         r = Register(1)
         self.assertEqual(r.read(), False)
 
-    def test_reg1_write(self):
+    def test_flag_write(self):
         r = Register(1)
         r.write(True)
         self.assertEqual(r.read(), True)
 
-    def test_reg1_badwrite(self):
+    def test_flag_trunc(self):
         r = Register(1)
-        with self.assertRaises(AssertionError):
-            r.write(2)
+        r.write(3)
+        self.assertEqual(r.read(), True)
