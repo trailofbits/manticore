b3b628198cdcb0b9f2f1f6523869ae811c59af32
yan <rottled@gmail.com>
2017-03-22 15:05:56 -0400

    move hooks to methods; cleanup

--- manticore/utils/emulate.py
+++ manticore/utils/emulate.py
@@ -18,14 +18,16 @@ from capstone.x86 import *
 logger = logging.getLogger("EMULATOR")
 
 class UnicornEmulator(object):
+    '''
+    Helper class to emulate a single instruction via Unicorn.
+    '''
     def __init__(self, cpu):
         self._cpu = cpu
         self._emu = self._unicorn()
         self._undo_list = []
-        self._written = []
         self._mapped = []
         self._to_raise = None
-        self._should_try_again = False
+        #self._should_try_again = False
 
     def _unicorn(self):
         if self._cpu.arch == CS_ARCH_ARM:
@@ -45,7 +47,7 @@ class UnicornEmulator(object):
         if m.start in self._mapped:
             return m
 
-        # We might be modifying memory, set all permissions
+        # We might be modifying memory (even read-only), set all permissions
         permissions = UC_PROT_WRITE | UC_PROT_READ | UC_PROT_EXEC
 
         uc.mem_map(m.start, len(m), permissions)
@@ -62,8 +64,81 @@ class UnicornEmulator(object):
                 return self._emu.reg_read(UC_X86_REG_RIP)
         
 
+    def _hook_xfer_mem(self, uc, access, address, size, value, data):
+        '''
+        Handle memory operations from unicorn.
+        '''
+        assert access in (UC_MEM_WRITE, UC_MEM_READ, UC_MEM_FETCH)
+
+        # Make sure the memory we're reading or writing is mapped
+        m = self._create_emulated_mapping(uc, address, size)
+
+        if access == UC_MEM_WRITE:
+            self._cpu.write_int(address, value, size*8)
+        elif access == UC_MEM_READ:
+            value = self._cpu.read_bytes(address, size)
+            uc.mem_write(address, ''.join(value))
+        else:
+            # XXX(yan): Does this need handling?
+            pass
+
+        return True
+
+
+    def _hook_read_unmapped(self, uc, access, address, size, value, data):
+        '''
+        We hit an unmapped region; map it into unicorn. If we tried to 
+        execute from a set of addresses to handle Linux intrinsics, raise
+        the appropriate exception.
+
+        '''
+
+        if not self._cpu.memory.access_ok(slice(address, address+size), 'r'):
+            # TODO(yan): handle this in a different place
+            if self._cpu.arch == CS_ARCH_ARM:
+                from ..models.linux import Linux
+                special_addrs = (Linux.ARM_GET_TLS, Linux.ARM_CMPXCHG, Linux.ARM_MEM_BARRIER)
+                if address in special_addrs:
+                    # We are trying to execute from a special ARM value;
+                    # map it so we don't keep hitting this hook
+                    uc.mem_map(address & ~0xFFF, 0x1000)
+
+                    from ..core.cpu.abstractcpu import InvalidPCException
+                    self._to_raise = InvalidPCException(address)
+                else:
+                    self._to_raise = MemoryException("Can't read at", address)
+
+            self._should_try_again = False
+            return False
+
+        # XXX(yan): handle if this points to an incorrect mapping
+        self._create_emulated_mapping(uc, address, size)
+
+        bytes = self._cpu.read_bytes(address, size)
+        uc.mem_write(address, ''.join(bytes))
+
+        self._should_try_again = True
+        return True
+
+    def _hook_write_unmapped(self, uc, access, address, size, value, data):
+        '''
+        If we're about to write to unmapped memory, map it in and try again.
+        '''
+        m = self._create_emulated_mapping(uc, address, size)
+        self._should_try_again = True
+        return True
+
+
+    def _interrupt(self, uc, number, data):
+        '''
+        Handle software interrupt
+        '''
+        from ..core.cpu.abstractcpu import Interruption
+        self._to_raise = Interruption(number)
+        return True
+
     def emulate(self, instruction):
-        def _reg_id(reg_name):
+        def _to_unicorn_id(reg_name):
             #FIXME FIXME FIXME
             #assert unicorn.__version__ <= '1.0.0', "If we are using unicorn greater than 1.0.0 we have ARM.APSR support
             #if unicorn.__version__ <= '1.0.0' and reg_name == 'APSR':
@@ -85,174 +160,67 @@ class UnicornEmulator(object):
             # The last 8 canonical registers of x86 are individual flags; replace
             # with the eflags
             registers -= set(['CF','PF','AF','ZF','SF','IF','DF','OF'])
+            registers.add('EFLAGS')
 
         for reg in registers:
             val = self._cpu.read_register(reg)
             if issymbolic(val):
                 raise ConcretizeRegister(reg, "Concretizing register for emulation.") 
-            self._emu.reg_write(_reg_id(reg), val)
+            self._emu.reg_write(_to_unicorn_id(reg), val)
 
-
-        #Unicorn hack. On single step unicorn wont advance the PC register
-        PC = self._cpu.PC
 
         m = self._create_emulated_mapping(self._emu, self._cpu.PC, instruction.size)
-        self._emu.mem_write(PC, ''.join(m[PC:PC+instruction.size]))
-        
+        text_bytes = self._cpu.read_bytes(self._cpu.PC, instruction.size)
+        self._emu.mem_write(self._cpu.PC, ''.join(text_bytes))
 
-        def _xfer_mem(uc, access, address, size, value, self):
-            # XXX(yan): record memory writes to our own state and undo them if
-            # we have to re-execute
-            #print("_xfer_mem: access({}) address({:x}) size({}) value({})"
-                    #.format(['write', 'read'][access == UC_MEM_READ], address, size, repr(value))
+        self._emu.hook_add(UC_HOOK_MEM_UNMAPPED,       self._hook_read_unmapped)
+        self._emu.hook_add(UC_HOOK_MEM_WRITE,          self._hook_xfer_mem)
+        self._emu.hook_add(UC_HOOK_MEM_WRITE_UNMAPPED, self._hook_write_unmapped)
+        self._emu.hook_add(UC_HOOK_MEM_READ,           self._hook_xfer_mem)
+        self._emu.hook_add(UC_HOOK_INTR,               self._interrupt)
 
-            assert access in (UC_MEM_WRITE, UC_MEM_READ, UC_MEM_FETCH)
+        saved_PC = self._cpu.PC
 
-            # Make sure the memory we're reading or writing is mapped
-            m = self._create_emulated_mapping(uc, address, size)
-
-            if access == UC_MEM_WRITE:
-                self._cpu.write_int(address, value, size*8)
-            elif access == UC_MEM_READ:
-                value = self._cpu.read_bytes(address, size)
-                uc.mem_write(address, ''.join(value))
-            else:
-                # XXX(yan): Does this need handling?
-                pass
-
-            return True
-
-        def _read_unmapped(uc, access, address, size, value, self):
-            '''
-            We hit an unmapped region; map it into unicorn
-            '''
-
-            if not self._cpu.memory.access_ok(slice(address, address+size), 'r'):
-                if self._cpu.arch == CS_ARCH_ARM:
-                    from ..models.linux import Linux
-                    if address in (Linux.ARM_GET_TLS, Linux.ARM_CMPXCHG,
-                            Linux.ARM_MEM_BARRIER):
-                        # We are trying to execute from a special ARM value;
-                        # map it so we don't keep hitting this hook
-                        uc.mem_map(address & ~0xFFF, 0x1000)
-
-                        from ..core.cpu.abstractcpu import InvalidPCException
-                        self._to_raise = InvalidPCException(address)
-                        self._should_try_again = False
-
-                        return False
-                self._to_raise = MemoryException("Can't read at", address)
-                self._should_try_again = False
-                return False
-
-            # XXX(yan): handle if this points to an incorrect mapping
-            self._create_emulated_mapping(uc, address, size)
-
-            bytes = self._cpu.read_bytes(address, size)
-            uc.mem_write(address, ''.join(bytes))
-
-            self._should_try_again = True
-            return True
-
-        def _write_unmapped(uc, access, address, size, value, self):
-            print "unmapped ({}, {}, {:x}, {}, {}, {})\n\n\n".format(repr(uc), repr(access), (address), repr(size), repr(value), repr(self))
-            m = self._create_emulated_mapping(uc, address, size)
-            self._written.append((address, size*8, value))
-            self._should_try_again = True
-            return True
-
-        def _invalid_write(uc, access, address, size, value, self):
-            print "invalid write"
-            self._should_try_again = False
-            return False
-
-
-        def _interrupt(uc, number, self):
-            from ..core.cpu.abstractcpu import Interruption
-            self._to_raise = Interruption(number)
-            return True
-
-        def _generic_hook(*args):
-            print "generic hook: ", args
-            return True
-
-        self._emu.hook_add(UC_HOOK_MEM_UNMAPPED,       _read_unmapped, self)
-        self._emu.hook_add(UC_HOOK_MEM_WRITE,          _xfer_mem, self)
-        self._emu.hook_add(UC_HOOK_MEM_WRITE_UNMAPPED, _write_unmapped, self)
-        self._emu.hook_add(UC_HOOK_MEM_READ,           _xfer_mem, self)
-        self._emu.hook_add(UC_HOOK_INTR,               _interrupt, self)
-
-        ctx = self._emu.context_save()
-
-        while True:
-            try:
+        try:
+            while True:
+                ctx = self._emu.context_save()
                 self._should_try_again = False
                 self._emu.emu_start(self._cpu.PC, self._cpu.PC+instruction.size, count=1)
+
                 if not self._should_try_again:
                     break
+
+                # If we are trying again, restore context
                 self._emu.emu_stop()
                 self._emu.context_restore(ctx)
 
-            except UcError as e:
-                if e.errno == UC_ERR_WRITE_UNMAPPED:
-                    raise MemoryException(repr(e), 0)
-                else:
-                    "got error: ", e
-
-        #sys.exit(0)
+        except UcError as e:
+            if e.errno == UC_ERR_WRITE_UNMAPPED:
+                raise MemoryException(repr(e), 0)
+            else:
+                # XXX(yan): We might need to raise here
+                pass
 
         if logger.getEffectiveLevel() == logging.DEBUG:
             logger.debug("="*10)
             for register in self._cpu.canonical_registers:
-                logger.debug("Register % 3s  Manticore: %08x, Unicorn %08x", register, self._cpu.read_register(register), self._emu.reg_read(_reg_id(register)) )
+                logger.debug("Register % 3s  Manticore: %08x, Unicorn %08x",
+                        register, self._cpu.read_register(register),
+                        self._emu.reg_read(_to_unicorn_id(register)) )
             logger.debug(">"*10)
-
-        
 
         # Bring back Unicorn registers to Manticore
         for reg in registers:
-            val = self._emu.reg_read(_reg_id(reg))
+            val = self._emu.reg_read(_to_unicorn_id(reg))
             self._cpu.write_register(reg, val)
-        
 
         #Unicorn hack. On single step unicorn wont advance the PC register
         mu_pc = self.get_unicorn_pc()
-        if PC == mu_pc:
-            #PC should have been updated by emulator :(
-            self._cpu.PC = self._cpu.PC+instruction.size
-        #else:
-            #print 'not bumping {:x} {:x}'.format(mu_pc, PC)
-            #self._cpu.PC = mu_pc
+        if saved_PC == mu_pc:
+            self._cpu.PC += instruction.size
 
         if self._to_raise:
             raise self._to_raise
 
         return
 
-
-## Tucked away here for later debugging  help
-##        if PC == 0x4008b0:
-##            def _generic_hook(*args):
-##                print "generic: ", args
-##                return True
-##            self._emu.hook_add(UC_HOOK_INTR,               _generic_hook, 0)
-##            self._emu.hook_add(UC_HOOK_INSN,               _generic_hook, 1)
-##            self._emu.hook_add(UC_HOOK_INSN,               _generic_hook, 1)
-##            self._emu.hook_add(UC_HOOK_CODE,               _generic_hook, 2)
-##            self._emu.hook_add(UC_HOOK_BLOCK,              _generic_hook, 3)
-##            self._emu.hook_add(UC_HOOK_MEM_READ_UNMAPPED,  _generic_hook, 4)
-##            self._emu.hook_add(UC_HOOK_MEM_WRITE_UNMAPPED, _generic_hook, 5)
-##            self._emu.hook_add(UC_HOOK_MEM_READ_PROT,      _generic_hook, 7)
-##            self._emu.hook_add(UC_HOOK_MEM_WRITE_PROT,     _generic_hook, 8)
-##            self._emu.hook_add(UC_HOOK_MEM_FETCH_PROT,     _generic_hook, 9)
-##            self._emu.hook_add(UC_HOOK_MEM_WRITE,          _generic_hook, 11)
-##            self._emu.hook_add(UC_HOOK_MEM_FETCH,          _generic_hook, 12)
-##            self._emu.hook_add(UC_HOOK_MEM_READ_AFTER,     _generic_hook, 13)
-##            self._emu.hook_add(UC_HOOK_MEM_UNMAPPED,       _generic_hook, 14)
-##            self._emu.hook_add(UC_HOOK_MEM_PROT,           _generic_hook, 15)
-##            self._emu.hook_add(UC_HOOK_MEM_READ_INVALID,   _generic_hook, 16)
-##            self._emu.hook_add(UC_HOOK_MEM_WRITE_INVALID,  _generic_hook, 17)
-##            self._emu.hook_add(UC_HOOK_MEM_FETCH_INVALID,  _generic_hook, 18)
-##            self._emu.hook_add(UC_HOOK_MEM_INVALID,        _generic_hook, 19)
-##            self._emu.hook_add(UC_HOOK_MEM_VALID,          _generic_hook, 20)
-##            
