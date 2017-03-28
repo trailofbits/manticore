703741ed0e492ae09f8040707015dee75ab61888
JP Smith <jp.smith@trailofbits.com>
2017-03-27 20:52:00 -0400

    bugfixes

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -16,7 +16,6 @@ def benchmark(program):
     m.should_profile = True
     m.run()
 
-    instructions_executed = m._executor.count
     results = m._executor.dump_stats()
     if results is None:
         print "[*] Failed to collect stats for program {}".format(program)
@@ -41,6 +40,7 @@ if __name__ == "__main__":
             results = benchmark(program)
             overall_results.time_elapsed += results.time_elapsed
             overall_results.instructions_executed += results.instructions_executed
+            overall_results.solver_time += results.solver_time
             overall_results.loading_time += results.loading_time
             overall_results.saving_time += results.saving_time
             
