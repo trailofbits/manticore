0fa4b8a01fad5398eab013fab9445bb0f4171edc
Mark Mossberg <mark@trailofbits.com>
2017-04-12 16:27:37 -0400

    Reimplement ceil so page aligned addresses are unaffected

--- manticore/core/memory.py
+++ manticore/core/memory.py
@@ -430,7 +430,7 @@ class Memory(object):
         :param address: the address to calculate its ceil.
         :return: the ceil of C{address}.
         '''
-        return ((address | self.page_mask) + 1 ) & self.memory_mask
+        return (((address-1)+self.page_size) & ~self.page_mask) & self.memory_mask
 
     def _floor(self, address):
         '''
