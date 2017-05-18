8840e31fc223de7275dc96e74424e0a19f0ad34a
Mark Mossberg <mark.mossberg@gmail.com>
2017-05-18 16:59:44 -0400

    Add strlen model (#260)
    * add models
    
    * wip strcmp tests
    
    * t
    
    * Add some tests
    
    * Better asserts
    
    * More pythonic
    
    * Add effectivene null test
    
    * init strlen
    
    * pythonic range
    
    * clean
    
    * docstring
    
    * tmp
    
    * stuff
    
    * stuff
    
    * Complete test
    
    * Add another test
    
    * Doc
    
    * clean
    
    * Add symbolic arg check
    
    * Fix tests
    
    * Make arch generic
    
    * Clean comment
    
    * Add basic unconstrained test

--- manticore/models.py
+++ manticore/models.py
@@ -84,3 +84,32 @@ def strcmp(state, s1, s2):
                 ret = 0
 
     return ret
+
+
+def strlen(state, s):
+    """
+    strlen symbolic model.
+
+    Algorithm: Walks from end of string not including NULL building ITE tree when current byte is symbolic.
+
+    :param State state: current program state
+    :param int s: Address of string
+    :return: Symbolic strlen result
+    :rtype: Expression or int
+    """
+
+    cpu = state.cpu
+
+    if issymbolic(s):
+        raise ConcretizeArgument(1)
+
+    zero_idx = _find_zero(cpu, state.constraints, s)
+
+    ret = zero_idx
+
+    for offset in xrange(zero_idx-1, -1, -1):
+        byt = cpu.read_int(s + offset, 8)
+        if issymbolic(byt):
+            ret = ITEBV(cpu.address_bit_size, byt == 0, offset, ret)
+
+    return ret
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,10 +4,10 @@ from manticore.core.smtlib import ConstraintSet, solver
 from manticore.core.state import State
 from manticore.platforms import linux
 
-from manticore.models import strcmp
+from manticore.models import strcmp, strlen
 
 
-class StrcmpTest(unittest.TestCase):
+class ModelTest(unittest.TestCase):
     l = linux.SLinux('/bin/ls')
     state = State(ConstraintSet(), l)
     stack_top = state.cpu.RSP
@@ -25,6 +25,8 @@ class StrcmpTest(unittest.TestCase):
         cpu.write_bytes(cpu.RSP, s)
         return cpu.RSP
 
+
+class StrcmpTest(ModelTest):
     def _push2(self, s1, s2):
         s1ptr = self._push_string(s1)
         s2ptr = self._push_string(s2)
@@ -99,3 +101,74 @@ class StrcmpTest(unittest.TestCase):
         self.state.constrain(s2[2] == ord('\0'))
         ret = strcmp(self.state, *strs)
         self.assertFalse(solver.can_be_true(self.state.constraints, ret != 0))
+
+
+class StrlenTest(ModelTest):
+    def test_concrete(self):
+        s = self._push_string('abc\0')
+        ret = strlen(self.state, s)
+        self.assertEqual(ret, 3)
+
+    def test_concrete_empty(self):
+        s = self._push_string('\0')
+        ret = strlen(self.state, s)
+        self.assertEqual(ret, 0)
+
+    def test_symbolic_effective_null(self):
+        sy = self.state.symbolicate_buffer('ab+')
+        self.state.constrain(sy[2] == 0)
+        s = self._push_string(sy)
+        ret = strlen(self.state, s)
+        self.assertEqual(ret, 2)
+
+    def test_symbolic(self):
+        sy = self.state.symbolicate_buffer('+++\0')
+        s = self._push_string(sy)
+
+        ret = strlen(self.state, s)
+        print solver.get_all_values(self.state.constraints, ret)
+        self.assertItemsEqual(range(4), solver.get_all_values(self.state.constraints, ret))
+
+        self.state.constrain(sy[0] == 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 0))
+        self._clear_constraints()
+
+        self.state.constrain(sy[0] != 0)
+        self.state.constrain(sy[1] == 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 1))
+        self._clear_constraints()
+
+        self.state.constrain(sy[0] != 0)
+        self.state.constrain(sy[1] != 0)
+        self.state.constrain(sy[2] == 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 2))
+        self._clear_constraints()
+
+        self.state.constrain(sy[0] != 0)
+        self.state.constrain(sy[1] != 0)
+        self.state.constrain(sy[2] != 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 3))
+
+    def test_symbolic_mixed(self):
+        sy = self.state.symbolicate_buffer('a+b+\0')
+        s = self._push_string(sy)
+
+        self.state.constrain(sy[1] == 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 1))
+        self._clear_constraints()
+
+        self.state.constrain(sy[1] != 0)
+        self.state.constrain(sy[3] == 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 3))
+        self._clear_constraints()
+
+        self.state.constrain(sy[1] != 0)
+        self.state.constrain(sy[3] != 0)
+        ret = strlen(self.state, s)
+        self.assertFalse(solver.can_be_true(self.state.constraints, ret != 4))
