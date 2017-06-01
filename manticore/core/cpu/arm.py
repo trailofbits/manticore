16149f6d0e80ff8e2fa2a1c17f25546d53dfd960
yan <rottled@gmail.com>
2017-06-01 13:45:15 -0400

    Add MOVT instruction

--- manticore/core/cpu/arm.py
+++ manticore/core/cpu/arm.py
@@ -510,6 +510,20 @@ class Armv7Cpu(Cpu):
         dest.write(result)
         cpu.setFlags(C=carry_out, N=HighBit(result), Z=(result == 0))
 
+    @instruction
+    def MOVT(cpu, dest, src):
+        '''
+        MOVT writes imm16 to Rd[31:16]. The write does not affect Rd[15:0].
+
+        :param Armv7Operand dest: The destination operand; register
+        :param Armv7Operand src: The source operand; 16-bit immediate
+        '''
+        assert src.type == 'immediate'
+        imm = src.read()
+        low_halfword = dest.read() & Mask(16)
+        dest.write((imm << 16) | low_halfword)
+
+
     def _compute_writeback(cpu, operand, offset):
         if offset:
             off = offset.read()
