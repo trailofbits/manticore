acb1c91b0f5301a58bff20d414143f2b80454128
yan <rottled@gmail.com>
2017-06-30 13:52:55 -0400

    Start bringing state serialization into workspace

--- manticore/core/workspace.py
+++ manticore/core/workspace.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import cPickle
 import logging
 import tempfile
 
@@ -10,14 +11,37 @@ from .smtlib.solver import SolverException
 
 logger = logging.getLogger('WORKSPACE')
 
+
+def _serialize_pickle(state, f):
+    '''
+    Serialize `state` to file object `f` using cPickle.
+
+    :param state:
+    :param f:
+    :return:
+    '''
+    try:
+        f.write(cPickle.dumps(state, 2))
+    except RuntimeError:
+        # recursion exceeded. try a slower, iterative solution
+        from ..utils import iterpickle
+        logger.warning("Using iterpickle to dump state")
+        f.write(iterpickle.dumps(state, 2))
+
+
 class Store(object):
     '''
     A Store can save arbitrary keys/values and file streams. Used for producing
     output, and state saving and loading.
     '''
 
-    def __init__(self, uri):
+    def __init__(self, uri, state_serialization_method='pickle'):
         self.uri = uri
+
+        if state_serialization_method == 'pickle':
+            self._serialize = lambda self, *args: _serialize_pickle(*args)
+        else:
+            raise NotImplementedError("Pickling method '{}' not supported.".format(state_serialization_method))
 
     def save_value(self, key, value):
         '''
@@ -60,6 +84,25 @@ class Store(object):
         '''
         raise NotImplementedError
 
+    def save_state(self, state, key):
+        '''
+        Save a state to storage.
+
+        :param state:
+        :return:
+        '''
+        with self.save_stream(key) as f:
+            self._method(state, f)
+
+
+    def load_state(self, key):
+        '''
+
+        :param state_id:
+        :return:
+        '''
+        pass
+
     def list_keys(self, prefix):
         '''
 
@@ -88,13 +131,14 @@ class FilesystemStore(Store):
         super(FilesystemStore, self).__init__(uri)
 
     @contextmanager
-    def save_stream(self, key):
+    def save_stream(self, key, binary=False):
         '''
 
         :param key:
         :return:
         '''
-        with open(os.path.join(self.uri, key), 'w') as f:
+        mode = 'w{}'.format('b' if binary else '')
+        with open(os.path.join(self.uri, key), mode) as f:
             yield f
 
     @contextmanager
@@ -129,7 +173,6 @@ class RedisStore(Store):
     def __init__(selfself, uri=None):
         pass
 
-
 def _create_store(desc):
     type, uri = ('fs', None) if desc is None else desc.split(':', 1)
 
@@ -144,6 +187,10 @@ class Workspace(object):
 
     def __init__(self, desc=None):
         self._store = _create_store(desc)
+        self._last_id = 0
+
+    def _try_loading_workspace(self):
+        pass
 
     def load_state(self, state_id):
         '''
@@ -155,23 +202,29 @@ class Workspace(object):
         '''
         raise NotImplementedError
 
-    def save_state(self, state):
+    def save_state(self, state, final=True):
         '''
         Save a state to storage, return identifier.
 
         :param state_id: The state reference of what to load
+        :param bool final: Whether the state is finalized (i.e. testcase)
         :return: The deserialized state
         :rtype: State
         '''
         raise NotImplementedError
 
-class Output(object):
+class ManticoreOutput(object):
     '''
     Base class for Manticore output. Responsible for generating execution-based
     output, such as state summaries, coverage information, etc.
     '''
 
     def __init__(self, desc=None):
+        '''
+        Create an object capable of producing Manticore output.
+
+        :param desc: A descriptor ('type:uri') of where to write output.
+        '''
         self._store = _create_store(desc)
 
     @property
@@ -199,12 +252,6 @@ class Output(object):
         self.save_fds(state)
 
     @contextmanager
-    def save_stream(self, key):
-        with self._store.save_stream(key) as s:
-            yield s
-
-
-    @contextmanager
     def _named_stream(self, name):
         '''
         Create an indexed output stream i.e. '00000001_name'
@@ -216,19 +263,17 @@ class Output(object):
             yield s
 
     def save_summary(self, state, message):
-        memories = set()
-
         with self._named_stream('messages') as summary:
             summary.write("Command line:\n  '{}'\n" .format(' '.join(sys.argv)))
             summary.write('Status:\n  {}\n\n'.format(message))
 
+            memories = set()
             for cpu in filter(None, state.platform.procs):
                 idx = state.platform.procs.index(cpu)
                 summary.write("================ PROC: %02d ================\n"%idx)
                 summary.write("Memory:\n")
                 if hash(cpu.memory) not in memories:
-                    for m in str(cpu.memory).split('\n'):
-                        summary.write("  %s\n"%m)
+                    summary.write(str(cpu.memory).replace('\n', '\n  '))
                     memories.add(hash(cpu.memory))
 
                 summary.write("CPU:\n{}".format(cpu))
@@ -264,16 +309,16 @@ class Output(object):
 
     def save_fds(self, state):
         with self._named_stream('stdout') as _out:
-            with self._named_stream('stdout') as _err:
-                with self._named_stream('stdin') as _in:
-                    for name, fd, data in state.platform.syscall_trace:
-                        if name in ('_transmit', '_write'):
-                            if   fd == 1: _out.write(''.join(str(c) for c in data))
-                            elif fd == 2: _err.write(''.join(str(c) for c in data))
-                        if name in ('_receive', '_read') and fd == 0:
-                            try:
-                                for c in data:
-                                    _in.write(chr(solver.get_value(state.constraints, c)))
-                            except SolverException, e:
-                                _in.write('{SolverException}')
+         with self._named_stream('stdout') as _err:
+          with self._named_stream('stdin') as _in:
+              for name, fd, data in state.platform.syscall_trace:
+                  if name in ('_transmit', '_write'):
+                      if   fd == 1: _out.write(''.join(str(c) for c in data))
+                      elif fd == 2: _err.write(''.join(str(c) for c in data))
+                  if name in ('_receive', '_read') and fd == 0:
+                      try:
+                          for c in data:
+                              _in.write(chr(solver.get_value(state.constraints, c)))
+                      except SolverException, e:
+                          _in.write('{SolverException}')
 
