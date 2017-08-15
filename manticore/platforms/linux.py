8adb97eeb1a458589cb577f8d4ef2ea742862786
yan <rottled@gmail.com>
2017-08-15 15:57:57 -0400

    Clean up SLinux.sys_open()

--- manticore/platforms/linux.py
+++ manticore/platforms/linux.py
@@ -2265,28 +2265,28 @@ class SLinux(Linux):
 
     def sys_open(self, buf, flags, mode):
         '''
+        A version of open(2) that includes a special case for a symbolic path.
+        When given a symbolic path, it will create a temporary file with 
+        64 bytes of symbolic bytes as contents and return that instead.
+
         :param buf: address of zero-terminated pathname
         :param flags: file access bits
         :param mode: file permission mode
         '''
         offset = 0
-        symbolic_path = False
-        if issymbolic(self.current.read_int(buf, 8)):
+        symbolic_path = issymbolic(self.current.read_int(buf, 8))
+        if symbolic_path:
             import tempfile
             fd, path = tempfile.mkstemp()
             with open(path, 'wb+') as f:
                 f.write('+'*64)
             self.symbolic_files.append(path)
-            symbolic_path = True
-            path_addr = self.current.memory.mmap(None, 1024, 'rw ')
-            self.current.write_bytes(path_addr, path)
-            buf = path_addr
+            buf = self.current.memory.mmap(None, 1024, 'rw ', data_init=path)
 
         rv = super(SLinux, self).sys_open(buf, flags, mode)
 
         if symbolic_path:
-            self.current.memory.munmap(path_addr, 1024)
-
+            self.current.memory.munmap(buf, 1024)
 
         return rv
 
