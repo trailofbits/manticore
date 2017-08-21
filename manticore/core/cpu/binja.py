14b4017a036fe258e9eef29dfcfef2df63da7c3a
Theofilos Petsios <theofilos.pe@gmail.com>
2017-08-21 18:23:52 -0400

    moved execute in binja and cleanup of self.instruction refs

--- manticore/core/cpu/binja.py
+++ manticore/core/cpu/binja.py
@@ -17,6 +17,7 @@ from ..smtlib import Operators, BitVecConstant, operator
 from ...utils.helpers import issymbolic
 
 logger = logging.getLogger("CPU")
+register_logger = logging.getLogger("REGISTERS")
 
 
 class BinjaRegisterFile(RegisterFile):
@@ -343,7 +344,7 @@ class BinjaCpu(Cpu):
             c = self.memory[address]
 
             if issymbolic(c):
-                assert isinstance(c, BitVec) and  c.size == 8
+                assert isinstance(c, BitVec) and c.size == 8
                 if isinstance(c, Constant):
                     c = chr(c.value)
                 else:
@@ -365,14 +366,6 @@ class BinjaCpu(Cpu):
         except StopIteration as e:
             raise DecodeException(pc, code)
 
-        if pc in self._instruction_cache and not self.disasm.il_queue:
-            insn = self._instruction_cache[pc]
-            if (isinstance(self.disasm, BinjaILDisasm) and
-                    isinstance(insn, cs.CsInsn)):
-                self.update_platform_cpu_regs()
-            return insn
-
-
         #Check that the decoded instruction is contained in executable memory
         if not self.memory.access_ok(slice(pc, pc + insn.size), 'x'):
             logger.info("Trying to execute instructions from non-executable memory")
@@ -386,10 +379,80 @@ class BinjaCpu(Cpu):
             self.update_platform_cpu_regs()
 
         insn.operands = self._wrap_operands(insn.operands)
-        if not self.disasm.il_queue:
-            self._instruction_cache[pc] = insn
-
         return insn
+
+    def execute(self):
+        '''
+        Decode, and execute one instruction pointed by register PC
+        '''
+        if issymbolic(self.PC):
+            raise ConcretizeRegister(self, 'PC', policy='ALL')
+
+        if not self.memory.access_ok(self.PC, 'x'):
+            raise InvalidMemoryAccess(self.PC, 'x')
+
+        self.publish('will_decode_instruction', self.PC)
+
+        insn = self.decode_instruction(self.PC)
+        self._last_pc = self.PC
+
+        self.publish('will_execute_instruction', insn)
+
+        # FIXME (theo) why just return here?
+        if insn.address != self.PC:
+            return
+
+        name = self.canonicalize_instruction_name(insn)
+        def fallback_to_emulate(*operands):
+            if (isinstance(self.disasm, BinjaILDisasm) and
+                    isinstance(insn, cs.CsInsn)):
+                # if we got a capstone instruction using BinjaILDisasm, it means
+                # this instruction is not implemented. Fallback to Capstone
+                self.FALLBACK(name, *operands)
+                # XXX after this point self.PC != self._last_pc but that is
+                # OK because we will update the PC  properly
+            else:
+                text_bytes = ' '.join('%02x'%x for x in insn.bytes)
+                logger.info("Unimplemented instruction: 0x%016x:\t%s\t%s\t%s",
+                            insn.address, text_bytes, insn.mnemonic, insn.op_str)
+
+                self.publish('will_emulate_instruction', insn)
+                self.emulate(insn)
+
+        implementation = getattr(self, name, fallback_to_emulate)
+
+        if logger.level == logging.DEBUG :
+            logger.debug(self.render_instruction(insn))
+            for l in self.render_registers():
+                register_logger.debug(l)
+
+        assert ((hasattr(insn, "sets_pc") and insn.sets_pc) or
+                self.PC == self._last_pc)
+
+        implementation(*insn.operands)
+
+        # In case we are executing IL instructions, we could iteratively
+        # invoke multiple instructions due to the tree form, thus we only
+        # want to increment the PC once, based on its previous position
+        # for CALLS and JUMPS the PC should have been set automatically
+        # so no need to do anything. Also, if there are pending instruction
+        if not isinstance(self.disasm, BinjaILDisasm):
+            return
+
+        # don't bump the PC if we are in an LLIL that has set it,
+        # or if there are pending IL insn in the queue. This is because
+        # for cases where we have other il instructions in the queue,
+        # such as when we get a divu insn, the PC + size will point
+        # to the next assembly instruction and not the next LLIL
+        #
+        # we might be executing a Capstone instruction at this point
+        # if we context-switched, so check the sets_pc attr
+        if not (hasattr(insn, "sets_pc") and
+                (insn.sets_pc or self.disasm.il_queue)):
+            self.PC = self._last_pc + insn.size
+
+        self._icount += 1
+        self.publish('did_execute_instruction', insn)
 
     def update_platform_cpu_regs(self):
         for pl_reg, binja_reg in self.regfile.pl2b_map.items():
@@ -473,37 +536,6 @@ class BinjaCpu(Cpu):
 
         return [BinjaOperand(self, self.disasm.disasm_il, op)
                 for op in operands]
-
-    def fallback_to_emulate(self, *operands):
-        if (isinstance(self.disasm, BinjaILDisasm) and
-                isinstance(self.instruction, cs.CsInsn)):
-            # if we got a capstone instruction using BinjaILDisasm, it means
-            # this instruction is not implemented. Fallback to Capstone
-            name = self.canonicalize_instruction_name(self.instruction)
-            self.FALLBACK(name, *operands)
-        else:
-            super(BinjaCpu, self).fallback_to_emulate(*operands)
-
-    def update_pc(self):
-        # In case we are executing IL instructions, we could iteratively
-        # invoke multiple instructions due to the tree form, thus we only
-        # want to increment the PC once, based on its previous position
-        # for CALLS and JUMPS the PC should have been set automatically
-        # so no need to do anything. Also, if there are pending instruction
-        if not isinstance(self.disasm, BinjaILDisasm):
-            return
-
-        # don't bump the PC if we are in an LLIL that has set it,
-        # or if there are pending IL insn in the queue. This is because
-        # for cases where we have other il instructions in the queue,
-        # such as when we get a divu insn, the PC + size will point
-        # to the next assembly instruction and not the next LLIL
-        #
-        # we might be executing a Capstone instruction at this point
-        # if we context-switched, so check the sets_pc attr
-        if not (hasattr(self.instruction, "sets_pc") and
-                (self.instruction.sets_pc or self.disasm.il_queue)):
-            self.PC = self._last_pc + self.instruction.size
 
     # XXX this is currently not active because a bunch of flag-setting
     # LLIL are not implemented by Binja :(
@@ -1236,6 +1268,11 @@ def x86_get_eflags(cpu, reg):
 def insn_flags(cpu):
     # FIXME temporary hack due to binja issue with flags
     f = cpu.disasm.current_func
+    #  print "------"
+    #  print hex(cpu.PC)
+    #  print f
+    #  print cpu.disasm.disasm_il
+    #  print hex(cpu.disasm.disasm_il.address)
     il = f.get_lifted_il_at(cpu.disasm.disasm_il.address)
     mod_flags = f.get_flags_written_by_lifted_il_instruction(il.instr_index)
     #  il2 = cpu.disasm.disasm_il
@@ -1256,6 +1293,7 @@ def x86_calculate_cmp_flags(cpu, size, res, left_v, right_v):
         's': _sign_flag(res, size),
         'o': _overflow_flag(res, right_v, left_v, size)
     }
+
     cpu.update_flags(flags)
 
 def x86_update_logic_flags(cpu, result, size):
