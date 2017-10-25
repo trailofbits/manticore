b0fb1a28cc1ef972f73e843fa74a16c7bca23ffb
yan <rottled@gmail.com>
2017-10-25 15:15:55 -0400

    Clean up driver script

--- examples/script/concolic.py
+++ examples/script/concolic.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+'''
+A simple concolic execution driver script. Only currently supports passing symbolic arguments via argv.
+
+'''
+
 import sys
 import time
-import struct
+import argparse
 import itertools
 
 from manticore import Manticore
-from manticore import models
 from manticore.core.plugin import ExtendedTracer, Follower, Plugin
 
-def partition(pred, iterable):
+def _partition(pred, iterable):
     t1, t2 = itertools.tee(iterable)
     return (list(itertools.ifilterfalse(pred, t1)), filter(pred, t2))
 
@@ -26,28 +30,47 @@ class TraceReceiver(Plugin):
     def will_generate_testcase_callback(self, state, test_id, msg):
         self._trace = state.context[self._tracer.context_key]
 
-        instructions, writes = partition(lambda x: x['type'] == 'regs', self._trace)
+        instructions, writes = _partition(lambda x: x['type'] == 'regs', self._trace)
         total = len(self._trace)
         print 'Recorded concrete trace: {}/{} instructions, {}/{} writes'.format(
             len(instructions), total, len(writes), total)
 
 
 
-# Create a concrete Manticore and record it
-m1 = Manticore.linux(sys.argv[1], ['--first', '--second', '-a', '123'])
-t = ExtendedTracer()
-r = TraceReceiver(t)
-m1.verbosity(2)
-m1.register_plugin(t)
-m1.register_plugin(r)
-m1.run()
+def main():
+    parser = argparse.ArgumentParser(description='Follow a concrete trace')
+    parser.add_argument('-f', '--explore_from', help='Value of PC from which to explore symbolically', type=str)
+    parser.add_argument('-t', '--explore_to', type=str, default=sys.maxint,
+                        help="Value of PC until which to explore symbolically. (Probably don't want this set)")
+    parser.add_argument('--verbose', '-v', action='count', help='Increase verbosity')
+    parser.add_argument('cmd', type=str, nargs='+',
+                        help='Program and arguments. Use "--" to separate script arguments from target arguments')
+    args = parser.parse_args(sys.argv[1:])
 
-time.sleep(10)
+    range = None
+    if args.explore_from:
+        range = (args.explore_from, args.explore_to)
 
-# Create a symbolic Manticore and follow last trace
-m2 = Manticore.linux(sys.argv[1], ['+++++++', '++++++++', '++', '+++'])
-f = Follower(r.trace)
-f.add_symbolic_range(0x400b22, 0x400ba7)
-m2.register_plugin(f)
-m2.verbosity(2)
-m2.run()
+    # Create a concrete Manticore and record it
+    m1 = Manticore.linux(args.cmd[0], args.cmd[1:])
+    t = ExtendedTracer()
+    r = TraceReceiver(t)
+    m1.verbosity(args.verbose)
+    m1.register_plugin(t)
+    m1.register_plugin(r)
+    m1.run(procs=1)
+
+    time.sleep(3)
+
+    # Create a symbolic Manticore and follow last trace
+    symbolic_args = ['+'*len(arg) for arg in args.cmd[1:]]
+    m2 = Manticore.linux(args.cmd[0], symbolic_args)
+    f = Follower(r.trace)
+    if range:
+        f.add_symbolic_range(*range)
+    m2.verbosity(args.verbose)
+    m2.register_plugin(f)
+    m2.run()
+
+if __name__=='__main__':
+    main()
