344f44a7771d9fc65b16006de556dc2b625ac69b
Cole Lightfighter <cole@onicsla.bz>
2017-11-09 21:26:56 -0700

    [fix] Issue #550: generates extra workspace folder
    Signed-off-by: Cole Lightfighter <cole@onicsla.bz>

--- examples/evm/seth.py
+++ examples/evm/seth.py
@@ -140,7 +140,7 @@ class ManticoreEVM(Manticore):
         #make the ethereum world state
         world = evm.EVMWorld(constraints)
         initial_state = State(constraints, world)
-        super(ManticoreEVM, self).__init__(initial_state)
+        super(ManticoreEVM, self).__init__(initial_state, workspace_url='mem:')
 
 
         #The following should go to manticore.context so we can use multiprocessing
