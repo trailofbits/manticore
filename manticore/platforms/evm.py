ee5f83fd717533c950e0d6f0eb3749598a0f1c85
feliam <felipe.andres.manzano@gmail.com>
2017-11-20 16:41:42 -0300

    More evm asm docs

--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -310,6 +310,31 @@ class EVMAssembler(object):
             :param description: textual description of the instruction
             :param operand: optional immediate operand
             :param offset: optional offset of this instruction in the program
+
+            Example use::
+
+                instruction = EVMAssembler.assemble_one('PUSH1 0x10')
+                print 'Instruction: %s'% instruction
+                print '\tdescription:', instruction.description
+                print '\tgroup:', instruction.group
+                print '\taddress:', instruction.offset
+                print '\tsize:', instruction.size
+                print '\thas_operand:', instruction.has_operand
+                print '\toperand_size:', instruction.operand_size
+                print '\toperand:', instruction.operand
+                print '\tsemantics:', instruction.semantics
+                print '\tpops:', instruction.pops
+                print '\tpushes:', instruction.pushes
+                print '\tbytes:', '0x'+instruction.bytes.encode('hex')
+                print '\twrites to stack:', instruction.writes_to_stack
+                print '\treads from stack:', instruction.reads_from_stack
+                print '\twrites to memory:', instruction.writes_to_memory
+                print '\treads from memory:', instruction.reads_from_memory
+                print '\twrites to storage:', instruction.writes_to_storage
+                print '\treads from storage:', instruction.reads_from_storage
+                print '\tis terminator', instruction.is_terminator
+
+
             '''
             self._opcode = opcode 
             self._name = name 
