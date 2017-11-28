350b04a535d3c060553919ab53cb7497f7d96914
root <root@localhost.localdomain>
2017-11-28 16:20:31 -0500

    travis tests include examples/

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -8,16 +8,17 @@ else
     RV=1
 fi
 
-echo "Testing all built examples"
 for i in $(find . -executable -type f); do
-	if manticore $i; then
-		echo "Success: " $i
-	else
-		echo "Failed : " $i
-		RV=1
+	if [[ $i  != *"sendmail"* ]]; then
+		if manticore $i; then
+			echo "Success: " $i
+		else
+			echo "Failed : " $i
+			RV=1
+		fi
 	fi
 done
-exit ${RV}
+
 cd ../..
 
 coverage erase
