c9134b18551184f6ddddc10ed0d032c690c47171
Mark Mossberg <mark@trailofbits.com>
2017-12-18 17:25:54 -0500

    Initial eth testing setup

---
+++ tests/test_eth.py
@@ -0,0 +1,18 @@
+import unittest
+import os
+
+from manticore.seth import ManticoreEVM, IntegerOverflow
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+from manticore.utils.log import init_logging
+init_logging()
+
+class EthDetectors(unittest.TestCase):
+    def test_int_ovf(self):
+        mevm = ManticoreEVM()
+        mevm.register_detector(IntegerOverflow())
+        filename = os.path.join(THIS_DIR, 'binaries/int_overflow.sol')
+        mevm.multi_tx_analysis(filename)
+        findings = mevm.global_findings
+        self.assertEqual(len(findings), 3)
