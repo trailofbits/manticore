9c0d0b80945e39449764f8a09838fdff1c4c79a5
feliam <felipe.andres.manzano@gmail.com>
2018-02-05 16:38:46 -0300

    better safety check + context fix

--- manticore/core/plugin.py
+++ manticore/core/plugin.py
@@ -28,7 +28,7 @@ class Plugin(object):
     def context(self):
         ''' Convenient access to shared context '''
         plugin_context_name = str(type(self))
-        if plugin_context_name not in plugin_context_name:
+        if plugin_context_name not in self.manticore.context:
             self.manticore.context[plugin_context_name] = {}
         return self.manticore.context[plugin_context_name]
 
