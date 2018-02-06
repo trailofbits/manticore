de7b509e0291650c85b5281cf33e14e6b501278f
feliam <felipe.andres.manzano@gmail.com>
2018-02-06 17:55:56 -0300

    Better ArrayProxy special case handling

--- manticore/core/smtlib/visitors.py
+++ manticore/core/smtlib/visitors.py
@@ -69,6 +69,9 @@ class Visitor(object):
         :param use_fixed_point: if True, it runs _methods until a fixed point is found
         :type use_fixed_point: Bool
         '''
+        if isinstance(node, ArrayProxy):
+            node = node.array
+
         cache = self._cache
 
         visited = set()
