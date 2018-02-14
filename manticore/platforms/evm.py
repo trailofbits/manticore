f44ea4b58d02a3b90442c4e5bdb03cf689cdf02a
Mark Mossberg <mark@trailofbits.com>
2018-02-14 16:27:44 -0500

    Fix typo

--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -1325,7 +1325,7 @@ class EVM(Eventful):
             self.last_exception = e
 
             # Technically, this is not the right place to emit these events because the
-            # instruction hasn't execute yet; it executes in the EVM platform class (EVMWorld).
+            # instruction hasn't executed yet; it executes in the EVM platform class (EVMWorld).
             # However, when I tried that, in the event handlers, `state.platform.current`
             # ends up being None, which caused issues. So, as a pragmatic solution, we emit
             # the event before technically executing the instruction.
