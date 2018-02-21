46e06042bd145b7058a1ead5f25d2727f9b59b05
disconnect3d <dominik.b.czarnota@gmail.com>
2018-02-22 00:15:51 +0100

    Fix V0-V31 regs, small refactor
    Cherry-picked from disconnect3d/aarch64/skeleton.

--- manticore/native/cpu/aarch64.py
+++ manticore/native/cpu/aarch64.py
@@ -7,8 +7,8 @@ from .register import Register
 
 
 class Aarch64RegisterFile(RegisterFile):
-    _R_REGS = tuple('R%d' % i for i in range(31))
-    _V_REGS = tuple('V%d' % i for i in range(31))
+    _R_REGS = tuple('R%d' % i for i in range(31))  # R0-R30 31 general-purpose registers
+    _V_REGS = tuple('V%d' % i for i in range(32))  # V0-V31 32 SIMD & FP registers
 
     def __init__(self):
         # TODO / FIXME: This is probably valid only for 'aarch32 state'
@@ -33,13 +33,13 @@ class Aarch64RegisterFile(RegisterFile):
         # * A 64-bit general-purpose register named X0 to X30
         # * A 32-bit general-purpose register named W0 to W30.
         alias_regs.update(
-            ((r, r.replace('R', 'X')) for r in self._R_REGS)
+            ('R%d' % i, 'X%d' % i) for i in range(len(self._R_REGS))
         )
 
         # V0-V31 32 SIMD&FP registers, V0 to V31.
         # Each of those can be accessed as 128-bit registers named Q0 to Q31.
         alias_regs.update(
-            ((v, v.replace('V', 'Q')) for v in self._V_REGS)
+            ('V%d' % i, 'Q%d' % i) for i in range(len(self._V_REGS))
         )
 
         super(Aarch64RegisterFile, self).__init__(alias_regs)
@@ -53,6 +53,8 @@ class Aarch64RegisterFile(RegisterFile):
         # 128 bit SIMD registers.
         for reg_name in self._V_REGS:
             self._regs[reg_name] = Register(128)
+
+        self._all_registers = super(Aarch64RegisterFile, self).all_registers + self._R_REGS + self._V_REGS
 
     def read(self, register):
         register = self._alias(register)
@@ -68,7 +70,7 @@ class Aarch64RegisterFile(RegisterFile):
 
     @property
     def all_registers(self):
-        return super(Aarch64RegisterFile, self).all_registers + self._R_REGS + self._V_REGS
+        return self._all_registers
 
 
 class Aarch64Cpu(Cpu):
