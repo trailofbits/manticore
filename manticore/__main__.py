3d56074b09e0a44d1a61f9613ae98cdbd2cde023
yan <rottled@gmail.com>
2018-02-22 10:48:34 -0500

    Bump version number

--- manticore/__main__.py
+++ manticore/__main__.py
@@ -60,7 +60,7 @@ def parse_arguments():
     parser.add_argument('--workspace', type=str, default=None,
                         help=("A folder name for temporaries and results."
                               "(default mcore_?????)"))
-    parser.add_argument('--version', action='version', version='Manticore 0.1.6',
+    parser.add_argument('--version', action='version', version='Manticore 0.1.7',
                          help='Show program version information')
     parser.add_argument('--txlimit', type=positive,
                         help='Maximum number of symbolic transactions to run (positive integer) (Ethereum only)')
