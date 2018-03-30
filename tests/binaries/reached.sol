ee666056b42f749f6537765e379b791e593fa0e5
feliam <felipe.andres.manzano@gmail.com>
2018-03-30 16:17:29 -0300

    Clean reachability test

--- tests/binaries/reached.sol
+++ tests/binaries/reached.sol
@@ -1,20 +1,17 @@
 contract Reachable {
     uint storage_variable;
-    event Log(string);
 
     function entry(uint input) {
         storage_variable = 0x585858;
-        this.write_then_throw();
+        //this.write_then_throw();
         this.call.gas(10000).value(msg.value)(bytes4(sha3("function write_then_throw()")));
 
         if (storage_variable == 0x585858){
-            Log("AAA");
             assembly {
                jump ( 0x4141414141414141414141414141414141414141 )
             }
         }
         else{ 
-            Log("BBB");
             assembly {
                jump ( 0x4242424242424242424242424242424242424242 )
             }
@@ -23,7 +20,6 @@ contract Reachable {
     }
 
     function write_then_throw() {
-        Log("ZZZ");
         storage_variable = 120;
         throw;
     }
