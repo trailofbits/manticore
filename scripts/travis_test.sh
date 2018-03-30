3c370e6493ae69d651ff3b7069d5398944896dc8
yan <rottled@gmail.com>
2018-03-30 18:14:52 -0400

    Disable resource warnings since theyre probably not relevant for testing

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -76,7 +76,10 @@ echo Ran example scripts
 popd
 
 coverage erase
-coverage run -m unittest discover tests/ 2>&1 >/dev/null | tee travis_tests.log
+
+# Disable resource warnings 
+PYTHONWARNINGS="ignore:ResourceWarning" coverage run -m unittest discover tests/ 2>&1 >/dev/null | tee travis_tests.log
+
 DID_OK=$(tail -n1 travis_tests.log)
 if [[ "${DID_OK}" == OK* ]]
 then
