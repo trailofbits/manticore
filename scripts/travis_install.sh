6fef3367080c56e79370daf29093a6848459785a
yan <rottled@gmail.com>
2018-03-30 14:52:12 -0400

    try bumping py version to 3.6

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -7,6 +7,6 @@ function install_solc {
 
 install_solc
 
-pip install -U pip
-pip uninstall -y Manticore || echo "Manticore not cached"  # uninstall any old, cached Manticore
-pip install --no-binary keystone-engine -e .[dev]  # ks can have pip install issues
+pip3 install -U pip
+pip3 uninstall -y Manticore || echo "Manticore not cached"  # uninstall any old, cached Manticore
+pip3 install --no-binary keystone-engine -e .[dev]  # ks can have pip install issues
