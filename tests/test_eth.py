a8ed55d1a2243116794534a0f4204b3446a529f9
Mark Mossberg <mark@trailofbits.com>
2018-04-03 17:06:35 -0600

    Port tests unrelated to concretizing dyn args

--- tests/test_eth.py
+++ tests/test_eth.py
@@ -1,3 +1,4 @@
+import struct
 import unittest
 import os
 
@@ -5,8 +6,7 @@ from manticore.core.plugin import Plugin
 from manticore.core.smtlib import ConstraintSet, operators
 from manticore.core.smtlib.expression import BitVec
 from manticore.core.state import State
-
-from manticore.ethereum import ManticoreEVM, IntegerOverflow, Detector, NoAliveStates
+from manticore.ethereum import ManticoreEVM, IntegerOverflow, Detector, NoAliveStates, ABI
 from manticore.platforms.evm import EVMWorld, ConcretizeStack, Create, concretized_args
 
 
@@ -16,6 +16,11 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 from manticore.utils.log import init_logging
 
 init_logging()
+
+def make_mock_evm_state():
+    cs = ConstraintSet()
+    fakestate = State(cs, EVMWorld(cs))
+    return fakestate
 
 
 class EthDetectorsIntegrationTest(unittest.TestCase):
@@ -31,16 +36,92 @@ class EthDetectorsIntegrationTest(unittest.TestCase):
         self.assertIn('overflow at MUL', all_findings)
 
 
+class EthereumAbiTests(unittest.TestCase):
+    def setUp(self):
+        self.state = make_mock_evm_state()
+
+    @staticmethod
+    def _pack_int_to_32(x):
+        return '\x00' * 28 + struct.pack('>I', x)
+
+    def test_dyn_address(self):
+        d = [
+            'AAAA',                    # function hash
+            self._pack_int_to_32(32),  # offset to data start
+            self._pack_int_to_32(2),   # data start; # of elements
+            self._pack_int_to_32(42),  # element 1
+            self._pack_int_to_32(43),  # element 2
+        ]
+        d = ''.join(d)
+
+        funcname, dynargs = ABI.parse(type_spec='func(address[])', data=d)
+
+        self.assertEqual(funcname, 'func')
+        self.assertEqual(dynargs, ([42, 43],))
+
+    def test_dyn_bytes(self):
+        d = [
+            'AAAA',                    # function hash
+            self._pack_int_to_32(32),  # offset to data start
+            self._pack_int_to_32(30),   # data start; # of elements
+            'Z'*30, '\x00'*2
+        ]
+        d = ''.join(d)
+
+        funcname, dynargs = ABI.parse(type_spec='func(bytes)', data=d)
+
+        self.assertEqual(funcname, 'func')
+        self.assertEqual(dynargs, ('Z'*30,))
+
+    def test_simple_types(self):
+        d = [
+            'AAAA',                    # function hash
+            self._pack_int_to_32(32),
+            '\xff' * 32,
+            '\xff'.rjust(32, '\0'),
+            self._pack_int_to_32(0x424242),
+            '\x7f' + '\xff' *31, # int256 max
+            '\x80'.ljust(32, '\0'), # int256 min
+
+
+        ]
+        d = ''.join(d)
+
+        funcname, dynargs = ABI.parse(type_spec='func(uint256,uint256,bool,address,int256,int256)', data=d)
+
+        self.assertEqual(funcname, 'func')
+        self.assertEqual(dynargs, (32, 2**256 - 1, 0xff, 0x424242, 2**255 - 1,-(2**255) ))
+
+    def test_mult_dyn_types(self):
+        d = [
+            'AAAA',                    # function hash
+            self._pack_int_to_32(0x40),  # offset to data 1 start
+            self._pack_int_to_32(0x80),  # offset to data 2 start
+            self._pack_int_to_32(10),  # data 1 size
+            'helloworld'.ljust(32, '\x00'), # data 1
+            self._pack_int_to_32(3),  # data 2 size
+            self._pack_int_to_32(3),  # data 2
+            self._pack_int_to_32(4),
+            self._pack_int_to_32(5),
+        ]
+        d = ''.join(d)
+
+        funcname, dynargs = ABI.parse(type_spec='func(bytes,address[])', data=d)
+
+        self.assertEqual(funcname, 'func')
+        self.assertEqual(dynargs, ('helloworld', [3, 4, 5]))
+
+    def test_self_make_and_parse_multi_dyn(self):
+        d = ABI.make_function_call('func', 'h'*50, [1, 1, 2, 2, 3, 3] )
+        d = ''.join(d)
+        funcname, dynargs = ABI.parse(type_spec='func(bytes,address[])', data=d)
+        self.assertEqual(funcname, 'func')
+        self.assertEqual(dynargs, ('h'*50, [1, 1, 2, 2, 3, 3]))
+
 class EthDetectorsTest(unittest.TestCase):
     def setUp(self):
         self.io = IntegerOverflow()
-        self.state = self.make_mock_evm_state()
-
-    @staticmethod
-    def make_mock_evm_state():
-        cs = ConstraintSet()
-        fakestate = State(cs, EVMWorld(cs))
-        return fakestate
+        self.state = make_mock_evm_state()
 
     def test_mul_no_overflow(self):
         """
@@ -76,6 +157,10 @@ class EthDetectorsTest(unittest.TestCase):
 
 class EthTests(unittest.TestCase):
     def test_emit_did_execute_end_instructions(self):
+        """
+        Tests whether the did_evm_execute_instruction event is fired for instructions that internally trigger
+        an exception
+        """
         class TestDetector(Detector):
             def did_evm_execute_instruction_callback(self, state, instruction, arguments, result):
                 if instruction.semantics in ('REVERT', 'STOP'):
