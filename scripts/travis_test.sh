af3a8e44d1c4e9f6c06a0708374f408a501b61cf
phoebe jenkins <feeeb@protonmail.com>
2018-04-19 15:57:07 +0000

    Make travis fail jobs for inadequate coverage

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -112,8 +112,8 @@ measure_cov() {
 
 #coverage report
 echo "Measuring code coverage..."
-measure_cov "manticore/core/smtlib/*" 80 || true
-measure_cov "manticore/core/cpu/x86.py" 50 || true
-measure_cov "manticore/core/memory.py" 85 || true
+measure_cov "manticore/core/smtlib/*" 80
+measure_cov "manticore/core/cpu/x86.py" 50
+measure_cov "manticore/core/memory.py" 85
 
 exit ${RV}
