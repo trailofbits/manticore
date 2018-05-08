84fca4cabffae399aacbcc7fe98cbbe2591ddf97
Pierre Pronchery <khorben@defora.org>
2018-05-09 01:26:01 +0200

    Avoid creating memory mappings with a negative size
    This should completely fix #479 this time.

--- manticore/platforms/linux.py
+++ manticore/platforms/linux.py
@@ -1309,7 +1309,7 @@ class Linux(Platform):
         :raises error:
                     - "Error in brk!" if there is any error allocating the memory
         '''
-        if brk != 0 and brk != self.elf_brk:
+        if brk != 0 and brk > self.elf_brk:
             mem = self.current.memory
             size = brk - self.elf_brk
             if brk > mem._ceil(self.elf_brk):
