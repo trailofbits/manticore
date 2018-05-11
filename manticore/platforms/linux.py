f6d0cd4e8e9c386ed48e1bbb30af88f801b8ae3a
Pierre Pronchery <khorben@defora.org>
2018-05-11 17:43:27 +0200

    Partially implement chroot(2) (#891)
    * Partially implement chroot(2)
    
    Really this return EPERM (permission denied), which seems to be absolutely fine
    since a non-privileged user is currently assumed. This is what would normally
    be returned in this scenario.
    
    * update chroot implementation

--- manticore/platforms/linux.py
+++ manticore/platforms/linux.py
@@ -1557,6 +1557,25 @@ class Linux(Platform):
 
         return newfd
 
+    def sys_chroot(self, path):
+        '''
+        An implementation of chroot that does perform some basic error checking,
+        but does not actually chroot.
+
+        :param path: Path to chroot
+        '''
+        if path not in self.current.memory:
+            return -errno.EFAULT
+
+        path_s = self.current.read_string(path)
+        if not os.path.exists(path_s):
+            return -errno.ENOENT
+
+        if not os.path.isdir(path_s):
+            return -errno.ENOTDIR
+
+        return -errno.EPERM
+
     def sys_close(self, fd):
         '''
         Closes a file descriptor
