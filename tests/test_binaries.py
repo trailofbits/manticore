1ae7b70b9b400f865e2e63409ac36f2a8142300f
yan <rottled@gmail.com>
2018-05-18 17:41:03 -0400

    Added brk test binary src

--- tests/test_binaries.py
+++ tests/test_binaries.py
@@ -173,6 +173,33 @@ class IntegrationTest(unittest.TestCase):
             self.assertIn("Message", f.read())
 
     def test_brk_regression(self):
+        """
+        Tests for brk behavior. Source of brk_static_amd64:
+
+	#include <stdio.h>
+	#include <unistd.h>
+	#include <stdint.h>
+
+	int main(int argc, char *argv[])
+	{
+	    uint8_t *p = sbrk(0);
+
+	    int valid_at_first = (p == sbrk(16));
+	    int valid_after_shift = ((p+16) == sbrk(0));
+	    sbrk(-16);
+	    int valid_after_reset = (p == sbrk(0));
+	    sbrk(-(2<<20));
+	    int valid_after_bad_brk = (p == sbrk(0));
+
+	    if (valid_at_first && valid_after_shift 
+		    && valid_after_reset && valid_after_bad_brk)
+		return 0;
+	    else
+		return 1;
+	}
+
+
+        """
         dirname = os.path.dirname(__file__)
         filename = os.path.abspath(os.path.join(dirname, 'binaries/brk_static_amd64'))
         workspace = '%s/workspace' % self.test_dir
