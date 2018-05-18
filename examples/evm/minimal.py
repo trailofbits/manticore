68c471687223397d5f352319c87a28f781e3005d
feliam <felipe.andres.manzano@gmail.com>
2018-05-18 18:14:34 -0300

    Remove solidity in minimal.py example (#895)
    * Remove all needs for solidity in minimal.py example
    
    * Separate evm only minimal example

--- examples/evm/minimal.py
+++ examples/evm/minimal.py
@@ -2,6 +2,7 @@ from manticore.ethereum import ManticoreEVM, evm
 ################ Script #######################
 
 m = ManticoreEVM()
+
 #And now make the contract account to analyze
 # cat  | solc --bin 
 source_code = '''
@@ -19,25 +20,19 @@ contract NoDistpatcher {
     } 
 }
 '''
+init_bytecode = m.compile(source_code)
 
 user_account = m.create_account(balance=1000)
 print "[+] Creating a user account", user_account
-
-init_bytecode = m.compile(source_code)
-print "[+] Source code:"
-print source_code
 
 print "[+] Init bytecode:", init_bytecode.encode('hex')
 print "[+] EVM init assembler:"
 for instr in evm.EVMAsm.disassemble_all(init_bytecode[:-44]):
     print hex(instr.offset), instr
 
-
-
 contract_account = m.create_contract(owner=user_account,
                                         init=init_bytecode)
 print "[+] Creating a contract account", contract_account
-
 
 print "[+] Now the symbolic values"
 symbolic_data = m.make_symbolic_buffer(320) 
