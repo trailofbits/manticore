410d1c15f526d7f96e283bed036f2a2d993eaec2
yan <rottled@gmail.com>
2018-06-05 18:31:16 -0400

    Move platform-dependent code out of the conditioinal

--- manticore/utils/mappings.py
+++ manticore/utils/mappings.py
@@ -6,75 +6,45 @@ mmap_function = None
 munmap_function = None
 
 # initialize global functions depending on platform
-osname = sys.platform.lower()
-if osname == "darwin" or osname.startswith("linux"):
-
-    if osname == "darwin":
-        libc = ctypes.cdll.LoadLibrary("libc.dylib")
+def get_libc():
+    osname = sys.platform.lower()
+    if osname.startswith("darwin"):
+        filename = "libc.dylib"
+    elif osname.startswith("linux"):
+        filename = "libc.so.6"
+    elif osname.startswith("netbsd"):
+        filename = "libc.so"
     else:
-        libc = ctypes.cdll.LoadLibrary("libc.so.6")
+        raise ValueError("Unsupported host OS: " + osname)
 
-    # void* mmap(void* addr, size_t len, int prot, int flags, int fd, off_t offset)
-    mmap_function = libc.mmap
-    mmap_function.restype = ctypes.c_void_p
-    mmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t,
-                             ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_size_t]
-    # int munmap(void* addr, size_t len)
-    munmap_function = libc.munmap
-    munmap_function.restype = ctypes.c_int
-    munmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t]
+    return ctypes.cdll.LoadLibrary(filename)
 
-    def _munmap(address, size):
-        result = munmap_function(address, size)
-        assert result == 0
+libc = get_libc()
 
-    def _mmap(fd, offset, size):
-        prot = MMAP.PROT_READ | MMAP.PROT_WRITE
-        flags = MMAP.MAP_PRIVATE
+# void* mmap(void* addr, size_t len, int prot, int flags, int fd, off_t offset)
+mmap_function = libc.mmap
+mmap_function.restype = ctypes.c_void_p
+mmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t,
+                         ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_size_t]
 
-        if size & 0xfff != 0:
-            size = (size & ~0xfff) + 0x1000
+# int munmap(void* addr, size_t len)
+munmap_function = libc.munmap
+munmap_function.restype = ctypes.c_int
+munmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t]
 
-        assert size > 0
+def mmap(fd, offset, size):
+    prot = MMAP.PROT_READ | MMAP.PROT_WRITE
+    flags = MMAP.MAP_PRIVATE
 
-        result = mmap_function(0, size, prot, flags, fd, offset)
-        return ctypes.cast(result, ctypes.POINTER(ctypes.c_char))
+    if size & 0xfff != 0:
+        size = (size & ~0xfff) + 0x1000
 
-    mmap = _mmap
-    munmap = _munmap
+    assert size > 0
 
-elif osname.startswith("netbsd"):
+    result = mmap_function(0, size, prot, flags, fd, offset)
+    return ctypes.cast(result, ctypes.POINTER(ctypes.c_char))
 
-    libc = ctypes.cdll.LoadLibrary("libc.so")
+def munmap(address, size):
+    result = munmap_function(address, size)
+    assert result == 0
 
-    # void* mmap(void* addr, size_t len, int prot, int flags, int fd, off_t offset)
-    mmap_function = libc.mmap
-    mmap_function.restype = ctypes.c_void_p
-    mmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t,
-                             ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_size_t]
-    # int munmap(void* addr, size_t len)
-    munmap_function = libc.munmap
-    munmap_function.restype = ctypes.c_int
-    munmap_function.argtype = [ctypes.c_void_p, ctypes.c_size_t]
-
-    def _munmap(address, size):
-        result = munmap_function(address, size)
-        assert result == 0
-
-    def _mmap(fd, offset, size):
-        prot = MMAP.PROT_READ | MMAP.PROT_WRITE
-        flags = MMAP.MAP_PRIVATE
-
-        if size & 0xfff != 0:
-            size = (size & ~0xfff) + 0x1000
-
-        assert size > 0
-
-        result = mmap_function(0, size, prot, flags, fd, offset)
-        return ctypes.cast(result, ctypes.POINTER(ctypes.c_char))
-
-    mmap = _mmap
-    munmap = _munmap
-
-else:
-    raise ValueError("Unsupported host OS: " + osname)
