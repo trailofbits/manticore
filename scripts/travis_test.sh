535bcdaf07ae81b3799b12278649ea0f2f5b4efd
yan <rottled@gmail.com>
2018-06-06 15:29:11 -0400

    Enable verbose output

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -76,7 +76,7 @@ popd
 coverage erase
 
 # Disable resource warnings 
-PYTHONWARNINGS="ignore:ResourceWarning" coverage run -m unittest discover tests/ 2>&1 >/dev/null | tee travis_tests.log
+PYTHONWARNINGS="ignore:ResourceWarning" coverage run -m unittest discover -v tests/ 2>&1 >/dev/null | tee travis_tests.log
 
 DID_OK=$(tail -n1 travis_tests.log)
 if [[ "${DID_OK}" == OK* ]]
