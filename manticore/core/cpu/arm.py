70b4fde9af18c413476651cb22e134c0f163ef37
yan <rottled@gmail.com>
2018-06-20 17:50:42 -0400

    typo

--- manticore/core/cpu/arm.py
+++ manticore/core/cpu/arm.py
@@ -754,7 +754,7 @@ class Armv7Cpu(Cpu):
 
         :param ARMv7Operand dest: the destination register; register
         :param ARMv7Operand dest: the source register; register
-        :param int src_width: bit to consider of the src operand
+        :param int src_width: bits to consider of the src operand
         """
         val = GetNBits(src.read(), src_width)
         word = Operators.ZEXTEND(val, cpu.address_bit_size)
