c88d91582bab720895b3f6e73e3f6da964728f92
yan <rottled@gmail.com>
2018-06-20 16:12:32 -0400

    Update UQSUB8 instruction to support symbolic values and add tests

--- manticore/core/cpu/arm.py
+++ manticore/core/cpu/arm.py
@@ -1263,10 +1263,12 @@ class Armv7Cpu(Cpu):
     def UQSUB8(cpu, dest, op1, op2):
         src1 = op1.read()
         src2 = op2.read()
-        result = 0
-        for i in (range(op1.size // 8)):
-            byteResult = ((src1 & (0xFF << (8 * i))) - (src2 & (0xFF << (8 * i))))
-            if byteResult < 0:
-                byteResult = 0
-            result = result | byteResult
-        dest.write(result)
+        result = []
+        for offset in reversed(range(0, op1.size, 8)):
+            byte1 = Operators.EXTRACT(src1, offset, 8)
+            byte2 = Operators.EXTRACT(src2, offset, 8)
+            byte_diff = byte1 - byte2
+            result.append(
+                    Operators.ITEBV(8, byte_diff < 0, 0, byte_diff))
+        dest.write(Operators.CONCAT(dest.size, *result))
+
