63ac45c26915d805546edda7d8b11252cb2978ff
Mark Mossberg <mark.mossberg@gmail.com>
2018-06-22 19:10:41 -0400

    Manticore 0.1.10 (#941)
    * Bump version number
    
    * Add changelog skeleton
    
    * Add changelog
    
    * Add missing detect-all flag
    
    * Don't forget --avoid-constant

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description='Manticore is a symbolic execution tool for analysis of binaries and smart contracts.',
     url='https://github.com/trailofbits/manticore',
     author='Trail of Bits',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     install_requires=[
         'capstone>=3.0.5rc2',
