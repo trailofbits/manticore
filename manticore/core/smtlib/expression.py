3f499225dbf8b02024eef38d7ea1f25a742208ce
feliam <felipe.andres.manzano@gmail.com>
2018-07-26 16:10:28 -0300

    ArrayProxy store() return itself

--- manticore/core/smtlib/expression.py
+++ manticore/core/smtlib/expression.py
@@ -898,7 +898,7 @@ class ArrayProxy(Array):
         self.written.add(index)
         auxiliar = self._array.store(index, value)
         self._array = auxiliar
-        return auxiliar
+        return self
 
     def __getitem__(self, index):
         if isinstance(index, slice):
