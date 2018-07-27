a44778c0d174b1a668fb1dd7a7ea295a6973d270
feliam <felipe.andres.manzano@gmail.com>
2018-07-27 18:51:45 -0300

    CC

--- manticore/core/smtlib/solver.py
+++ manticore/core/smtlib/solver.py
@@ -369,7 +369,7 @@ class Z3Solver(Solver):
 
             temp_cs.add(var == expression)
             self._reset(temp_cs.to_string(related_to=var))
-            
+
             result = []
             val = None
             while self._check() == 'sat':
