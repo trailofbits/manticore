99f158aa3576218b5bb8a229d9afa98f31c7cb9c
Yan Ivnitskiy <yan@users.noreply.github.com>
2018-07-30 19:36:34 -0400

    Report test coverage to CodeClimate (#1004)
    This PR enables the reporting of test coverage of all the test jobs (`eth` and `tests`) to CodeClimate. This uses S3 to temporarily store results between jobs and later upload them to CC.
    
    Fixes #1000

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -5,16 +5,33 @@ function install_solc {
     sudo chmod +x /usr/bin/solc
 }
 
-install_solc
+function install_mcore {
+    pip install -U pip
+    pip uninstall -y Manticore || echo "Manticore not cached"  # uninstall any old, cached Manticore
 
-pip install -U pip
-pip uninstall -y Manticore || echo "Manticore not cached"  # uninstall any old, cached Manticore
 
-# We only need to install keystone if we're just running regular tests
-EXTRAS="dev-noks"
-if [ "$1" = "tests" ]; then
-    EXTRAS="dev"
+    # We only need to install keystone if we're just running regular tests
+    EXTRAS="dev-noks"
+    if [ "$1" = "tests" ]; then
+        EXTRAS="dev"
+    fi
+
+    pip install --no-binary keystone-engine -e .[$EXTRAS]  # ks can have pip install issues
+}
+
+function install_cc_env {
+    curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+    chmod +x ./cc-test-reporter
+
+    pip install awscli
+}
+
+# install CodeClimate env in all conditions
+install_cc_env
+
+if [ "$1" != "env" ]; then
+    install_solc
+    install_mcore $1
 fi
 
-pip install --no-binary keystone-engine -e .[$EXTRAS]  # ks can have pip install issues
 
