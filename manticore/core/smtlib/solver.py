1cebf94e9b107be85f3db181e40e4bc8844a4b1c
Mark Mossberg <mark@trailofbits.com>
2018-08-07 15:47:09 -0400

    2 hour timeout on this branch

--- manticore/core/smtlib/solver.py
+++ manticore/core/smtlib/solver.py
@@ -147,7 +147,9 @@ class Z3Solver(Solver):
         else:
             logger.debug(' Please install Z3 4.4.1 or newer to get optimization support')
 
-        self._command = 'z3 -t:240000 -memory:16384 -smt2 -in'
+        # self._command = 'z3 -t:240000 -memory:16384 -smt2 -in'  # original
+        minutes = 120
+        self._command = 'z3 -t:{} -memory:16384 -smt2 -in'.format(minutes * 60 * 1000)
         self._init = ['(set-logic QF_AUFBV)', '(set-option :global-decls false)']
         self._get_value_fmt = (re.compile('\(\((?P<expr>(.*))\ #x(?P<value>([0-9a-fA-F]*))\)\)'), 16)
 
