78e2ddf510b79d63a2dccf3cfa8e22e3d206a4e2
defunct <defunct@defunct.io>
2018-08-07 15:17:27 -0400

    resolves #992 (#1033)
    * resolves #992

--- manticore/ethereum.py
+++ manticore/ethereum.py
@@ -1398,8 +1398,7 @@ class ManticoreEVM(Manticore):
     def get_account(self, name):
         return self._accounts[name]
 
-    # TODO: FIXME: multiprocessing is broken, procs>1 results in erroneous results
-    def __init__(self, procs=1, **kwargs):
+    def __init__(self, procs=10, **kwargs):
         ''' A Manticore EVM manager
             :param int procs: number of workers to use in the exploration
         '''
