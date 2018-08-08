6d58b7b7d246de8acb6774c1cb4e4c2a1714dafd
defunct <defunct@defunct.io>
2018-08-08 17:58:33 +0000

    dead code elimination, __init__ cleanup

--- manticore/core/smtlib/__init__.py
+++ manticore/core/smtlib/__init__.py
@@ -6,18 +6,3 @@ from . import operators as Operators  # noqa
 
 import logging
 logger = logging.getLogger(__name__)
-
-'''
-class OperationNotPermited(SolverException):
-    def __init__(self):
-        super().__init__("You cant build this expression")     #no childrens
-
-class ConcretizeException(SolverException):
-    def __init__(self, expression):
-        super().__init__("Need to concretize the following and retry\n"+str(expression))     #no childrens
-        self.expression = expression
-'''
-
-
-class VisitorException(Exception):
-    pass
