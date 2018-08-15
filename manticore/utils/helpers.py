5684bdd9fde17a567de9037aba458baa50394178
defunct <defunct@defunct.io>
2018-08-15 16:49:40 -0400

    Code cleanup and coverage (#1035)
    * dead code elimination, __init__ cleanup
    * `binary.Elf` bugfix, add `binary` package tests

--- manticore/utils/helpers.py
+++ manticore/utils/helpers.py
@@ -72,38 +72,6 @@ def taint_with(arg, taint, value_bits=256, index_bits=256):
     return arg
 
 
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args, **kwargs):
-        key = args + tuple(sorted(kwargs.items()))
-        if not isinstance(key, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args, **kwargs)
-        if key in self.cache:
-            return self.cache[key]
-        else:
-            value = self.func(*args, **kwargs)
-            self.cache[key] = value
-            return value
-
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
-
-
 class CacheDict(OrderedDict):
     def __init__(self, *args, max_size=30000, flush_perc=30, **kwargs):
         self._max_size = max_size
