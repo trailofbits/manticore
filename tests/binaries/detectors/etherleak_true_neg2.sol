6d61bbb009f4999f05c10512b315b015ad5ebd46
Mark Mossberg <mark@trailofbits.com>
2018-08-23 19:54:11 -0400

    Add another neg

---
+++ tests/binaries/detectors/etherleak_true_neg2.sol
@@ -0,0 +1,31 @@
+/*
+   Example contract - True Negative
+   The ether leak is reachable by non-creator if you set yourself as the owner, BUT the balance
+   can never be > 0.
+
+   This should NOT report a finding.
+*/
+
+pragma solidity ^0.4.24;
+
+contract DetectThis {
+
+  address private owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function fakeconstructor() {
+    owner = msg.sender;
+  }
+
+  function () payable { // makes it possible for contract to have balance > 0
+  }
+
+  function withdrawfunds() onlyOwner {
+    msg.sender.transfer(this.balance);
+  }
+
+}
