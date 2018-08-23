1df2974d24c0b9b6334ddb20ecf92f0ecb2493f1
Mark Mossberg <mark@trailofbits.com>
2018-08-23 19:51:55 -0400

    Initial test

---
+++ tests/binaries/detectors/etherleak_true_pos_argument.sol
@@ -0,0 +1,30 @@
+/*
+   Example contract - True Positive
+   The argument ether leak is reachable by non-creator if you set yourself as the owner.
+
+   This should report a finding.
+*/
+
+pragma solidity ^0.4.24;
+
+contract DetectThis {
+
+  address private owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function fakeconstructor() {
+    owner = msg.sender;
+  }
+
+  function () payable { // makes it possible for contract to have balance > 0
+  }
+
+  function withdrawfunds(address attacker) onlyOwner {
+    attacker.transfer(this.balance);
+  }
+
+}
---
+++ tests/binaries/detectors/etherleak_true_pos_argument1.sol
@@ -0,0 +1,41 @@
+/*
+   Example contract - True Positive, Potential false negative
+   The ether leak is reachable by non-creator if you set yourself as the owner by exploiting
+   the set() function.
+
+   NOTE: this currently requires the LoopDepthLimiter plugin to find quickly. This is due to the
+   assignment to map.length.
+
+   This should report a finding.
+*/
+
+pragma solidity ^0.4.24;
+
+contract DetectThis {
+
+  address private owner;
+  uint256[] map;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+    
+  function set(uint256 key, uint256 value) public { // you can use this to overwrite owner
+    // Expand dynamic array as needed
+    if (map.length <= key) {
+      map.length = key + 1;
+    }
+
+    map[key] = value;
+  }
+
+
+  function () payable { // makes it possible for contract to have balance > 0
+  }
+
+  function withdrawfunds(address attacker) onlyOwner {
+    attacker.transfer(this.balance);
+  }
+
+}
