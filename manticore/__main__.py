41bad1eb5b598cfe2da648d58d165acfb15f8489
feliam <felipe.andres.manzano@gmail.com>
2018-09-01 16:17:06 -0300

    manticore 0.2.1.1 (#1116)

--- manticore/__main__.py
+++ manticore/__main__.py
@@ -62,7 +62,7 @@ def parse_arguments():
     parser.add_argument('--workspace', type=str, default=None,
                         help=("A folder name for temporaries and results."
                               "(default mcore_?????)"))
-    parser.add_argument('--version', action='version', version='Manticore 0.2.1-b',
+    parser.add_argument('--version', action='version', version='Manticore 0.2.1.1',
                         help='Show program version information')
     parser.add_argument('--txlimit', type=positive,
                         help='Maximum number of symbolic transactions to run (positive integer) (Ethereum only)')
