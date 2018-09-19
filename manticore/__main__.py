836bd9a41d0e81e85602b9d414208a6b0f4e0bcf
feliam <felipe.andres.manzano@gmail.com>
2018-09-19 11:37:28 -0300

    DetectReentrancyAdvanced from cli (#1132)
    * Fix commandline access so DetectReentrancyAdvanced is accessible
    
    * Fix cli arguments to support advanced reentrancy detector

--- manticore/__main__.py
+++ manticore/__main__.py
@@ -94,6 +94,9 @@ def parse_arguments():
     parser.add_argument('--detect-reentrancy', action='store_true',
                         help='Enable detection of reentrancy bug (Ethereum only)')
 
+    parser.add_argument('--detect-reentrancy-advanced', action='store_true',
+                        help='Enable detection of reentrancy bug -- this detector is better used via API (Ethereum only)')
+
     parser.add_argument('--detect-unused-retval', action='store_true',
                         help='Enable detection of unused internal transaction return value (Ethereum only)')
 
@@ -134,7 +137,7 @@ def parse_arguments():
 
 
 def ethereum_cli(args):
-    from .ethereum import ManticoreEVM, DetectInvalid, DetectIntegerOverflow, DetectUninitializedStorage, DetectUninitializedMemory, FilterFunctions, DetectReentrancySimple, DetectUnusedRetVal, DetectSelfdestruct, LoopDepthLimiter, DetectDelegatecall, DetectExternalCallAndLeak, DetectReentrancySimple, DetectEnvInstruction
+    from .ethereum import ManticoreEVM, DetectInvalid, DetectIntegerOverflow, DetectUninitializedStorage, DetectUninitializedMemory, FilterFunctions, DetectReentrancySimple, DetectUnusedRetVal, DetectSelfdestruct, LoopDepthLimiter, DetectDelegatecall, DetectExternalCallAndLeak, DetectReentrancyAdvanced, DetectEnvInstruction
 
     log.init_logging()
 
@@ -150,6 +153,8 @@ def ethereum_cli(args):
         m.register_detector(DetectUninitializedMemory())
     if args.detect_all or args.detect_reentrancy:
         m.register_detector(DetectReentrancySimple())
+    if args.detect_reentrancy_advanced:
+        m.register_detector(DetectReentrancyAdvanced())
     if args.detect_all or args.detect_unused_retval:
         m.register_detector(DetectUnusedRetVal())
     if args.detect_all or args.detect_delegatecall:
