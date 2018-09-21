d4ab20bff3771870c7875b9d72cb69b9c8fb6d7a
Mark Mossberg <mark@trailofbits.com>
2018-09-21 12:57:43 -0400

    fix imports

--- tests/test_x86_pcmpxstrx.py
+++ tests/test_x86_pcmpxstrx.py
@@ -2,7 +2,7 @@
 import unittest
 import functools
 from manticore.core.cpu.x86 import *
-from manticore.core.smtlib import Operators
+from manticore.core.smtlib import Operators, Expression
 from manticore.core.memory import *
 
 
