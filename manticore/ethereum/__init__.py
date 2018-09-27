b6bee682a96b5e42a9318786dfba4e20bc75f926
Mark Mossberg <mark@trailofbits.com>
2018-09-27 17:32:22 -0400

    port everything over

--- manticore/ethereum/__init__.py
+++ manticore/ethereum/__init__.py
@@ -6,6 +6,7 @@ import os
 import pyevmasm as EVMAsm
 
 from .. import Manticore
+# from ..manticore import Manticore
 from ..exceptions import EthereumError, DependencyError, NoAliveStates
 from ..core.smtlib import ConstraintSet, Operators, solver, BitVec, Array, ArrayProxy
 from ..platforms import evm
@@ -1349,83 +1350,86 @@ class ManticoreEVM(Manticore):
         for sym_tx in blockchain.human_transactions:  # external transactions
             stream.write("Transactions No. %d\n" % blockchain.transactions.index(sym_tx))
 
-            conc_tx = sym_tx.concretize(state)
+            is_something_symbolic = sym_tx.dump(stream, state, self, flagged)
 
-            # The result if any RETURN or REVERT
-            stream.write("Type: %s (%d)\n" % (sym_tx.sort, sym_tx.depth))
 
-            caller_solution = conc_tx.caller
-
-            caller_name = self.account_name(caller_solution)
-            stream.write("From: %s(0x%x) %s\n" % (caller_name, caller_solution, flagged(issymbolic(sym_tx.caller))))
-
-            address_solution = conc_tx.address
-            address_name = self.account_name(address_solution)
-
-            stream.write("To: %s(0x%x) %s\n" % (address_name, address_solution, flagged(issymbolic(sym_tx.address))))
-            stream.write("Value: %d %s\n" % (conc_tx.value, flagged(issymbolic(sym_tx.value))))
-            stream.write("Gas used: %d %s\n" % (conc_tx.gas, flagged(issymbolic(sym_tx.gas))))
-
-            tx_data = conc_tx.data
-
-            stream.write("Data: 0x{} {}\n".format(binascii.hexlify(tx_data).decode(), flagged(issymbolic(sym_tx.data))))
-
-            if sym_tx.return_data is not None:
-                # return_data = state.solve_one(tx.return_data)
-                return_data = conc_tx.return_data
-
-                stream.write("Return_data: 0x{} {}\n".format(binascii.hexlify(return_data).decode(), flagged(issymbolic(sym_tx.return_data))))
-
-            metadata = self.get_metadata(sym_tx.address)
-            if sym_tx.sort == 'CREATE':
-                if metadata is not None:
-
-                    conc_args_data = conc_tx.data[len(metadata._init_bytecode):]
-                    arguments = ABI.deserialize(metadata.get_constructor_arguments(), conc_args_data)
-
-                    # TODO confirm: arguments should all be concrete?
-
-                    is_argument_symbolic = any(map(issymbolic, arguments))  # is this redundant since arguments are all concrete?
-                    stream.write('Function call:\n')
-                    stream.write("Constructor(")
-                    stream.write(','.join(map(repr, map(state.solve_one, arguments))))  # is this redundant since arguments are all concrete?
-                    stream.write(') -> %s %s\n' % (sym_tx.result, flagged(is_argument_symbolic)))
-
-            if sym_tx.sort == 'CALL':
-                if metadata is not None:
-                    calldata = conc_tx.data
-                    is_calldata_symbolic = issymbolic(sym_tx.data)
-
-                    function_id = calldata[:4]  # hope there is enough data
-                    signature = metadata.get_func_signature(function_id)
-                    function_name = metadata.get_func_name(function_id)
-                    if signature:
-                        _, arguments = ABI.deserialize(signature, calldata)
-                    else:
-                        arguments = (calldata,)
-
-                    return_data = None
-                    if sym_tx.result == 'RETURN':
-                        ret_types = metadata.get_func_return_types(function_id)
-                        return_data = conc_tx.return_data
-                        return_values = ABI.deserialize(ret_types, return_data)  # function return
-
-                    is_return_symbolic = issymbolic(sym_tx.return_data)
-
-                    stream.write('\n')
-                    stream.write("Function call:\n")
-                    stream.write("%s(" % function_name)
-                    stream.write(','.join(map(repr, arguments)))
-                    stream.write(') -> %s %s\n' % (sym_tx.result, flagged(is_calldata_symbolic)))
-
-                    if return_data is not None:
-                        if len(return_values) == 1:
-                            return_values = return_values[0]
-
-                        stream.write('return: %r %s\n' % (return_values, flagged(is_return_symbolic)))
-                    is_something_symbolic = is_calldata_symbolic or is_return_symbolic
-
-            stream.write('\n\n')
+            # conc_tx = sym_tx.concretize(state)
+            #
+            # # The result if any RETURN or REVERT
+            # stream.write("Type: %s (%d)\n" % (sym_tx.sort, sym_tx.depth))
+            #
+            # caller_solution = conc_tx.caller
+            #
+            # caller_name = self.account_name(caller_solution)
+            # stream.write("From: %s(0x%x) %s\n" % (caller_name, caller_solution, flagged(issymbolic(sym_tx.caller))))
+            #
+            # address_solution = conc_tx.address
+            # address_name = self.account_name(address_solution)
+            #
+            # stream.write("To: %s(0x%x) %s\n" % (address_name, address_solution, flagged(issymbolic(sym_tx.address))))
+            # stream.write("Value: %d %s\n" % (conc_tx.value, flagged(issymbolic(sym_tx.value))))
+            # stream.write("Gas used: %d %s\n" % (conc_tx.gas, flagged(issymbolic(sym_tx.gas))))
+            #
+            # tx_data = conc_tx.data
+            #
+            # stream.write("Data: 0x{} {}\n".format(binascii.hexlify(tx_data).decode(), flagged(issymbolic(sym_tx.data))))
+            #
+            # if sym_tx.return_data is not None:
+            #     # return_data = state.solve_one(tx.return_data)
+            #     return_data = conc_tx.return_data
+            #
+            #     stream.write("Return_data: 0x{} {}\n".format(binascii.hexlify(return_data).decode(), flagged(issymbolic(sym_tx.return_data))))
+            #
+            # metadata = self.get_metadata(sym_tx.address)
+            # if sym_tx.sort == 'CREATE':
+            #     if metadata is not None:
+            #
+            #         conc_args_data = conc_tx.data[len(metadata._init_bytecode):]
+            #         arguments = ABI.deserialize(metadata.get_constructor_arguments(), conc_args_data)
+            #
+            #         # TODO confirm: arguments should all be concrete?
+            #
+            #         is_argument_symbolic = any(map(issymbolic, arguments))  # is this redundant since arguments are all concrete?
+            #         stream.write('Function call:\n')
+            #         stream.write("Constructor(")
+            #         stream.write(','.join(map(repr, map(state.solve_one, arguments))))  # is this redundant since arguments are all concrete?
+            #         stream.write(') -> %s %s\n' % (sym_tx.result, flagged(is_argument_symbolic)))
+            #
+            # if sym_tx.sort == 'CALL':
+            #     if metadata is not None:
+            #         calldata = conc_tx.data
+            #         is_calldata_symbolic = issymbolic(sym_tx.data)
+            #
+            #         function_id = calldata[:4]  # hope there is enough data
+            #         signature = metadata.get_func_signature(function_id)
+            #         function_name = metadata.get_func_name(function_id)
+            #         if signature:
+            #             _, arguments = ABI.deserialize(signature, calldata)
+            #         else:
+            #             arguments = (calldata,)
+            #
+            #         return_data = None
+            #         if sym_tx.result == 'RETURN':
+            #             ret_types = metadata.get_func_return_types(function_id)
+            #             return_data = conc_tx.return_data
+            #             return_values = ABI.deserialize(ret_types, return_data)  # function return
+            #
+            #         is_return_symbolic = issymbolic(sym_tx.return_data)
+            #
+            #         stream.write('\n')
+            #         stream.write("Function call:\n")
+            #         stream.write("%s(" % function_name)
+            #         stream.write(','.join(map(repr, arguments)))
+            #         stream.write(') -> %s %s\n' % (sym_tx.result, flagged(is_calldata_symbolic)))
+            #
+            #         if return_data is not None:
+            #             if len(return_values) == 1:
+            #                 return_values = return_values[0]
+            #
+            #             stream.write('return: %r %s\n' % (return_values, flagged(is_return_symbolic)))
+            #         is_something_symbolic = is_calldata_symbolic or is_return_symbolic
+            #
+            # stream.write('\n\n')
 
         if is_something_symbolic:
             stream.write('\n\n(*) Example solution given. Value is symbolic and may take other values\n')
--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -67,10 +67,92 @@ class Transaction(object):
         conc_data = state.solve_one(self.data)
         conc_return_data = state.solve_one(self.return_data)
 
-        # is it ok if these are None and get solve_one'd?
-
         return Transaction(self.sort, conc_address, self.price, conc_data, conc_caller, conc_value, conc_gas,
                            depth=self.depth, result=self.result, return_data=bytearray(conc_return_data))
+
+    def dump(self, stream, state, mevm, flagged):
+        from ..ethereum import ABI  # circular imports
+
+        is_something_symbolic = False
+
+        conc_tx = self.concretize(state)
+
+        # The result if any RETURN or REVERT
+        stream.write("Type: %s (%d)\n" % (self.sort, self.depth))
+
+        caller_solution = conc_tx.caller
+
+        caller_name = mevm.account_name(caller_solution)
+        stream.write("From: %s(0x%x) %s\n" % (caller_name, caller_solution, flagged(issymbolic(self.caller))))
+
+        address_solution = conc_tx.address
+        address_name = mevm.account_name(address_solution)
+
+        stream.write("To: %s(0x%x) %s\n" % (address_name, address_solution, flagged(issymbolic(self.address))))
+        stream.write("Value: %d %s\n" % (conc_tx.value, flagged(issymbolic(self.value))))
+        stream.write("Gas used: %d %s\n" % (conc_tx.gas, flagged(issymbolic(self.gas))))
+
+        tx_data = conc_tx.data
+
+        stream.write("Data: 0x{} {}\n".format(binascii.hexlify(tx_data).decode(), flagged(issymbolic(self.data))))
+
+        if self.return_data is not None:
+            # return_data = state.solve_one(tx.return_data)
+            return_data = conc_tx.return_data
+
+            stream.write("Return_data: 0x{} {}\n".format(binascii.hexlify(return_data).decode(), flagged(issymbolic(self.return_data))))
+
+        metadata = mevm.get_metadata(self.address)
+        if self.sort == 'CREATE':
+            if metadata is not None:
+
+                conc_args_data = conc_tx.data[len(metadata._init_bytecode):]
+                arguments = ABI.deserialize(metadata.get_constructor_arguments(), conc_args_data)
+
+                # TODO confirm: arguments should all be concrete?
+
+                is_argument_symbolic = any(map(issymbolic, arguments))  # is this redundant since arguments are all concrete?
+                stream.write('Function call:\n')
+                stream.write("Constructor(")
+                stream.write(','.join(map(repr, map(state.solve_one, arguments))))  # is this redundant since arguments are all concrete?
+                stream.write(') -> %s %s\n' % (self.result, flagged(is_argument_symbolic)))
+
+        if self.sort == 'CALL':
+            if metadata is not None:
+                calldata = conc_tx.data
+                is_calldata_symbolic = issymbolic(self.data)
+
+                function_id = calldata[:4]  # hope there is enough data
+                signature = metadata.get_func_signature(function_id)
+                function_name = metadata.get_func_name(function_id)
+                if signature:
+                    _, arguments = ABI.deserialize(signature, calldata)
+                else:
+                    arguments = (calldata,)
+
+                return_data = None
+                if self.result == 'RETURN':
+                    ret_types = metadata.get_func_return_types(function_id)
+                    return_data = conc_tx.return_data
+                    return_values = ABI.deserialize(ret_types, return_data)  # function return
+
+                is_return_symbolic = issymbolic(self.return_data)
+
+                stream.write('\n')
+                stream.write("Function call:\n")
+                stream.write("%s(" % function_name)
+                stream.write(','.join(map(repr, arguments)))
+                stream.write(') -> %s %s\n' % (self.result, flagged(is_calldata_symbolic)))
+
+                if return_data is not None:
+                    if len(return_values) == 1:
+                        return_values = return_values[0]
+
+                    stream.write('return: %r %s\n' % (return_values, flagged(is_return_symbolic)))
+                is_something_symbolic = is_calldata_symbolic or is_return_symbolic
+
+        stream.write('\n\n')
+        return is_something_symbolic
 
 
     @property
