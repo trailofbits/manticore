7b4d96d08004dcad3aa07b9c39afe6ee64864e3f
yan <rottled@gmail.com>
2018-10-08 17:40:11 -0400

    Move process_config_values to config.py

--- manticore/__main__.py
+++ manticore/__main__.py
@@ -17,43 +17,6 @@ consts.add('recursionlimit', default=10000,
 sys.setrecursionlimit(consts.recursionlimit)
 
 
-def process_config_values(args, parser):
-    """
-    Bring in provided config values to the args parser, and import entries to the config
-    from all arguments that were actually passed on the command line
-    """
-
-    # First, load a local config file, if passed or look for one in pwd if it wasn't.
-    config.load_overrides(args.config)
-
-    # Get a list of defined config vals. If these are passed on the command line,
-    # update them in their correct group, not in the cli group
-    defined_vars = list(config.get_config_keys())
-
-    command_line_args = vars(args)
-
-    # Bring in the options keys into args
-    config_cli_args = config.get_group('cli')
-
-    # Place all command line args into the cli group (for saving in the workspace). If
-    # the value is set on command line, then it takes precedence; otherwise we try to
-    # read it from the config file's cli group.
-    for k in command_line_args:
-        default = parser.get_default(k)
-        set_val = getattr(args, k)
-        if default is not set_val:
-            if k not in defined_vars:
-                config_cli_args.update(k, value=set_val)
-            else:
-                # Update a var's native group
-                group_name, key = k.split('.')
-                group = config.get_group(group_name)
-                setattr(group, key, set_val)
-        else:
-            if k in config_cli_args:
-                setattr(args, k, getattr(config_cli_args, k))
-
-
 def parse_arguments():
     def positive(value):
         ivalue = int(value)
@@ -180,7 +143,7 @@ def parse_arguments():
     if parsed.procs <= 0:
         parsed.procs = 1
 
-    process_config_values(parsed, parser)
+    config.process_config_values(parser, parsed)
 
     if parsed.config_print:
         print(config.describe_options())
--- manticore/utils/config.py
+++ manticore/utils/config.py
@@ -212,6 +212,46 @@ def add_config_vars_to_argparse(args):
                               default=obj.default, help=obj.description)
 
 
+def process_config_values(parser, args):
+    """
+    Bring in provided config values to the args parser, and import entries to the config
+    from all arguments that were actually passed on the command line
+
+    :param argparse.ArgumentParser parser: The arg parser
+    :param args: The value that parser.parse_args returned
+    """
+
+    # First, load a local config file, if passed or look for one in pwd if it wasn't.
+    load_overrides(args.config)
+
+    # Get a list of defined config vals. If these are passed on the command line,
+    # update them in their correct group, not in the cli group
+    defined_vars = list(get_config_keys())
+
+    command_line_args = vars(args)
+
+    # Bring in the options keys into args
+    config_cli_args = get_group('cli')
+
+    # Place all command line args into the cli group (for saving in the workspace). If
+    # the value is set on command line, then it takes precedence; otherwise we try to
+    # read it from the config file's cli group.
+    for k in command_line_args:
+        default = parser.get_default(k)
+        set_val = getattr(args, k)
+        if default is not set_val:
+            if k not in defined_vars:
+                config_cli_args.update(k, value=set_val)
+            else:
+                # Update a var's native group
+                group_name, key = k.split('.')
+                group = get_group(group_name)
+                setattr(group, key, set_val)
+        else:
+            if k in config_cli_args:
+                setattr(args, k, getattr(config_cli_args, k))
+
+
 def get_config_keys():
     """
     Return an iterable covering all defined keys so far
