c7ccf687be8f5f9a4c7d592fb8e21eb04a7391ab
Srinivas <spg349@nyu.edu>
2018-10-22 08:34:36 -0700

    update __init__.py to import STDIN_INPUT_DEFAULT_SIZE

--- manticore/__init__.py
+++ manticore/__init__.py
@@ -1,3 +1,3 @@
-from .manticore import Manticore  # noqa
+from .manticore import Manticore, STDIN_INPUT_DEFAULT_SIZE  # noqa
 from .models import variadic  # noqa
 from .utils.helpers import issymbolic  # noqa
