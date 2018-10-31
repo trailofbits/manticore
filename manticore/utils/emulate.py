c005aab3e11ccf159dabbb593fdae84906d9a96a
Alexandre Adamski <aadamski@quarkslab.com>
2018-10-31 19:45:59 +0100

    Fix emulation being performed in the wrong mode
    After the merge of the PR referenced at the end of this message into the master
    branch of Unicorn in July 2016, the emulation mode is set according to the
    least significant of bit of the program counter when it is updated.
    
    When an instruction is not implemented at Manticore's level and emulation is
    used, the PC value passed to the emu_start() function will trigger a switch of
    the current mode, setting it back to ARM mode instead of Thumb mode. This commit
    fixes this issue by ensuring that the least significant bit of the PC is set
    when Thumb mode emulation is performed.
    
    See https://github.com/unicorn-engine/unicorn/pull/592

--- manticore/utils/emulate.py
+++ manticore/utils/emulate.py
@@ -248,7 +248,10 @@ class UnicornEmulator(object):
         saved_PC = self._cpu.PC
 
         try:
-            self._emu.emu_start(self._cpu.PC, self._cpu.PC + instruction.size, count=1)
+            pc = self._cpu.PC
+            if self._uc_mode == UC_MODE_THUMB:
+                pc |= 1
+            self._emu.emu_start(pc, self._cpu.PC + instruction.size, count=1)
         except UcError as e:
             # We request re-execution by signaling error; if we we didn't set
             # _should_try_again, it was likely an actual error
