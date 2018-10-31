d3fabc97b40994af6dd24e326857ba5bf3a84c7c
disconnect3d <dominik.b.czarnota@gmail.com>
2018-10-31 16:55:03 +0100

    WIP

--- setup.py
+++ setup.py
@@ -21,26 +21,32 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     python_requires='>=3.6',
     install_requires=[
-        'capstone>=3.0.5',
-        'pyelftools',
-        'unicorn',
-        'ply',
-        'pysha3',
-        # In 0.1.1, pyevmasm changed its gas cost calculations, so Manticore will need to update its
-        # unit tests to match before we can upgrade pyevmasm
-        'pyevmasm==0.1.0',
         'pyyaml',
     ] + rtd_dependent_deps(),
     dependency_links=[
         'https://github.com/aquynh/capstone/archive/next.zip#egg=capstone-4&subdirectory=bindings/python',
     ],
     extras_require={
+        'native': [
+            'capstone>=3.0.5',
+            'pyelftools',
+            'unicorn',
+            'ply',
+        ],
+        'eth': [
+            'pysha3',
+            # In 0.1.1, pyevmasm changed its gas cost calculations, so Manticore will need to update its
+            # unit tests to match before we can upgrade pyevmasm
+            'pyevmasm==0.1.0',
+
+        ],
         'dev': [
             'keystone-engine',
             'coverage',
             'nose',
             'Sphinx',
         ],
+        # noks - no keystone
         'dev-noks': [
             'coverage',
             'nose',
