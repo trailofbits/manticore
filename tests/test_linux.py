55375079ae2a839cab3af680cd67fd40097f6cf7
Alexandre Adamski <aadamski@quarkslab.com>
2018-11-03 12:47:04 +0100

    Refactored the unit test to remove nested code

--- tests/test_linux.py
+++ tests/test_linux.py
@@ -276,7 +276,11 @@ class LinuxTest(unittest.TestCase):
         pickle.dumps(platform)
 
     def test_thumb_mode_entrypoint(self):
+        # thumb_mode_entrypoint is a binary with only one instruction
+        #   0x1000: add.w   r0, r1, r2
+        # which is a Thumb instruction, so the entrypoint is set to 0x1001
         m = Manticore.linux(os.path.join(os.path.dirname(__file__), 'binaries', 'thumb_mode_entrypoint'))
+        m.success = True
 
         @m.init
         def init(state):
@@ -284,11 +288,19 @@ class LinuxTest(unittest.TestCase):
             state.cpu.regfile.write('R1', 0x1234)
             state.cpu.regfile.write('R2', 0x5678)
 
-            @m.hook(None)
-            def success(state):
-                if state.cpu.PC < 0x1004:
-                    return
-                self.assertEqual(state.cpu.regfile.read('R0'), 0x68ac)
-                state.abandon()
+        @m.hook(0x1001)
+        def pre(state):
+            # If the wrong PC value was used by the loader (0x1001 instead of 0x1000),
+            # the wrong instruction bytes will have been fetched from memory
+            m.success = False
+            state.abandon()
+
+        @m.hook(0x1004)
+        def post(state):
+            # If the wrong execution mode was set by the loader, the wrong instruction
+            # will have been executed, so the register value will be incorrect
+            m.success = state.cpu.regfile.read('R0') == 0x68ac
+            state.abandon()
 
         m.run()
+        self.assertTrue(m.success)
