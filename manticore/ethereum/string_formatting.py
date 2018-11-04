3f9e8bf3b43d8ba45523a8516161fcfb558c68b3
yeti-detective <yeti.detective@gmail.com>
2018-11-04 16:39:45 -0500

    fix trailing whitespace in docstring detected by pep8 check

--- manticore/ethereum/string_formatting.py
+++ manticore/ethereum/string_formatting.py
@@ -7,6 +7,6 @@ def contract_addr(address):
 def evm_program_counter(pc, at_init=""):
     """
     Return string indicating EVM program counter and whether counter was read
-    at constructor 
+    at constructor
     """
     return 'EVM Program counter: 0x{}{}\n'.format(pc, at_init and " (at constructor)" or "")
