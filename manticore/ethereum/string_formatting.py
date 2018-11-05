1b878988472e26b674553ec73892d131b76117d2
yeti-detective <yeti.detective@gmail.com>
2018-11-05 16:26:23 -0500

    small change because CI failed, need to push again

--- manticore/ethereum/string_formatting.py
+++ manticore/ethereum/string_formatting.py
@@ -2,7 +2,7 @@ def contract_addr(address):
     """
     Return string indicating contact address
     """
-    return f'Contract: 0x{address}'
+    return F'Contract: 0x{address}'
 
 
 def evm_program_counter(pc, at_init=""):
@@ -10,4 +10,4 @@ def evm_program_counter(pc, at_init=""):
     Return string indicating EVM program counter and whether counter was read
     at constructor
     """
-    return f'EVM Program counter: 0x{pc}{" (at constructor)" if at_init else ""}\n'
+    return F'EVM Program counter: 0x{pc}{" (at constructor)" if at_init else ""}\n'
