fe0e3c97522647716c835ad6246af86408195ae6
yeti-detective <yeti.detective@gmail.com>
2018-11-05 13:49:58 -0500

    changing format strings to f strings

--- manticore/ethereum/string_formatting.py
+++ manticore/ethereum/string_formatting.py
@@ -2,7 +2,7 @@ def contract_addr(address):
     """
     Return string indicating contact address
     """
-    return 'Contract: 0x{}'.format(address)
+    return f'Contract: 0x{address}'
 
 
 def evm_program_counter(pc, at_init=""):
@@ -10,4 +10,4 @@ def evm_program_counter(pc, at_init=""):
     Return string indicating EVM program counter and whether counter was read
     at constructor
     """
-    return 'EVM Program counter: 0x{}{}\n'.format(pc, at_init and " (at constructor)" or "")
+    return f'EVM Program counter: 0x{pc}{at_init and " (at constructor)" or ""}\n'
