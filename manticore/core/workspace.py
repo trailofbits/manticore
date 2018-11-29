243cb1d09a761772ce410bdbdc163d6730d1e3a0
disconnect3d <dominik.b.czarnota@gmail.com>
2018-11-29 10:03:53 +0100

    Fix types in docstrings

--- manticore/core/workspace.py
+++ manticore/core/workspace.py
@@ -125,7 +125,7 @@ class Store(object):
         """
         Save a state to storage.
 
-        :param manticore.core.BaseState state:
+        :param manticore.core.StateBase state:
         :param str key:
         :return:
         """
@@ -137,7 +137,7 @@ class Store(object):
         Load a state from storage.
 
         :param key: key that identifies state
-        :rtype: manticore.core.BaseState
+        :rtype: manticore.core.StateBase
         """
         with self.load_stream(key, binary=True) as f:
             state = self._serializer.deserialize(f)
