60d84cf4293fb4587a3cfb3e7b8dddccc46d19da
yan <rottled@gmail.com>
2018-11-29 11:33:23 -0500

    Move symmem flag to bin_flags

--- manticore/__main__.py
+++ manticore/__main__.py
@@ -54,8 +54,6 @@ def parse_arguments():
     parser.add_argument('--workspace', type=str, default=None,
                         help=("A folder name for temporaries and results."
                               "(default mcore_?????)"))
-    parser.add_argument('--pure-symbolic', action='store_true',
-                        help='Treat all writable memory as symbolic')
     parser.add_argument('--version', action='version', version='Manticore 0.2.2',
                         help='Show program version information')
     parser.add_argument('--config', type=str,
@@ -76,6 +74,8 @@ def parse_arguments():
                            help='Specify symbolic input file, \'+\' marks symbolic bytes')
     bin_flags.add_argument('--env', type=str, nargs=1, default=[], action='append',
                            help='Add an environment variable. Use "+" for symbolic bytes. (VARNAME=++++)')
+    bin_flags.add_argument('--pure-symbolic', action='store_true',
+                           help='Treat all writable memory as symbolic')
 
     eth_flags = parser.add_argument_group('Ethereum flags')
     eth_flags.add_argument('--verbose-trace', action='store_true',
