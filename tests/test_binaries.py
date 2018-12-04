638cf7b1632e7f721fa3cf9a2239af0f896a12e9
disconnect3d <dominik.b.czarnota@gmail.com>
2018-12-04 16:13:52 +0100

    Fix amd/arm visited assertions

--- tests/test_binaries.py
+++ tests/test_binaries.py
@@ -55,13 +55,13 @@ class IntegrationTest(unittest.TestCase):
         # Remove the directory after the test
         shutil.rmtree(self.test_dir)
 
-    def _load_visited(self, visited):
+    def _load_visited_set(self, visited):
         self.assertTrue(os.path.exists(visited))
 
         with open(visited, 'r') as f:
             vitems = f.read().splitlines()
 
-        return [int(x[2:], 16) for x in vitems]
+        return set(int(x[2:], 16) for x in vitems)
 
     def _simple_cli_run(self, filename, contract=None, tx_limit=1, in_directory=None):
         """
@@ -164,33 +164,40 @@ class IntegrationTest(unittest.TestCase):
         self.assertIn(b'Results in ', output_lines[4])
         self.assertIn(b'Total time: ', output_lines[5])
 
-    def _test_arguments_assertions_aux(self, binname, refname, testcases_number):
+    def _test_arguments_assertions_aux(self, binname, testcases_number, visited, add_assertion=False):
         filename = os.path.abspath(os.path.join(DIRPATH, 'binaries', binname))
 
         self.assertTrue(filename.startswith(os.getcwd()))
 
         filename = filename[len(os.getcwd()) + 1:]
         workspace = '%s/workspace' % self.test_dir
-        assertions = '%s/assertions.txt' % self.test_dir
-
-        with open(assertions, 'w') as f:
-            f.write('0x0000000000401003 ZF == 1')
 
         cmd = [
             PYTHON_BIN, '-m', 'manticore',
             '--workspace', workspace,
             '--proc', '4',
             '--no-color',
-            '--assertions', assertions,
+        ]
+
+        # Only for amd64 binary/case
+        if add_assertion:
+            assertions = '%s/assertions.txt' % self.test_dir
+
+            with open(assertions, 'w') as f:
+                f.write('0x0000000000401003 ZF == 1')
+
+            cmd += ['--assertions', assertions]
+
+        cmd += [
             filename,
             '+++++++++',
         ]
 
         output = subprocess.check_output(cmd).splitlines()
 
-        self.assertIn(b'm.manticore:INFO: Verbosity set to 1.', output[0])
+        self.assertIn(b'm.c.manticore:INFO: Verbosity set to 1.', output[0])
 
-        self.assertIn(b'm.manticore:INFO: Loading program', output[1])
+        self.assertIn(b'm.n.manticore:INFO: Loading program', output[1])
         self.assertIn(bytes(binname, 'utf-8'), output[1])  # the binname should be in the path
 
         for i in range(testcases_number):
@@ -198,27 +205,75 @@ class IntegrationTest(unittest.TestCase):
 
             # After `expected1` there's the testcase id; because we fork use `--proc 4`
             # it might not be in the increasing order
-            expected1 = b'm.manticore:INFO: Generated testcase No. '
+            expected1 = b'm.c.manticore:INFO: Generated testcase No. '
             expected2 = b'- Program finished with exit status: '
 
             self.assertIn(expected1, line)
             self.assertIn(expected2, line)
 
-        self.assertIn(b'm.manticore:INFO: Results in /tmp', output[2+testcases_number])
-        self.assertIn(b'm.manticore:INFO: Total time: ', output[2+testcases_number+1])
+        self.assertIn(b'm.c.manticore:INFO: Results in /tmp', output[2+testcases_number])
+        self.assertIn(b'm.c.manticore:INFO: Total time: ', output[2+testcases_number+1])
 
-        actual = self._load_visited(os.path.join(DIRPATH, workspace, 'visited.txt'))
-        expected = self._load_visited(os.path.join(DIRPATH, 'reference', refname))
+        actual = self._load_visited_set(os.path.join(DIRPATH, workspace, 'visited.txt'))
 
-        self.assertEqual(actual, expected)
+        self.assertLess(set(visited), actual)
+
+        # this differs locally and on travis, but it is a good value that works on both...
+        self.assertGreater(len(actual), 2000)
+
+        self.assertEqual(len(set(visited)), len(visited))  # just a sanity check
 
     def test_arguments_assertions_amd64(self):
-        self._test_arguments_assertions_aux('arguments_linux_amd64', 'arguments_linux_amd64_visited.txt',
-                                            testcases_number=1)
+        self._test_arguments_assertions_aux('arguments_linux_amd64', testcases_number=1, visited=[
+            0x00400e40,  # _start
+            0x00400e64,  # call in _start
+            0x00401040,  # called function (let's name it A)
+            0x00401087,  # branch (je instruction)
+            0x004011c2,  # je taken
+            0x00401096,  # branches merge here
+            0x004010ca,  # end of the block,
+            0x004010d0,  # next block
+            0x004010db,  # `call B` instruction
+            0x004369b0,  # called function [has switch], let's name it B
+
+            # Switch cases in B:
+            #      0xa,        0x9,        0x2,        0x0,        0x8,        0x3,
+            0x00436a20, 0x00436a30, 0x00436a40, 0x00436a50, 0x00436a60, 0x00436a70,
+            #      0xe,        0xd,        0xb,       0x16,
+            0x00436aa0, 0x00436ab8, 0x00436ad0, 0x00436ae0,
+
+            0x00436b57,  # ret in B
+
+            0x00401148,  # A should eventually branch and get to this branch
+            0x004012c5,  # end of A
+
+            0x004002d8,  # _init
+            0x004002f1,  # ret in _init
+            0x0049124c,  # _fini
+            0x00491254,  # ret in _fini
+        ])
 
     def test_arguments_assertions_armv7(self):
-        self._test_arguments_assertions_aux('arguments_linux_armv7', 'arguments_linux_armv7_visited.txt',
-                                            testcases_number=19)
+        self._test_arguments_assertions_aux('arguments_linux_armv7', testcases_number=19, visited=[
+            0x00008b98,  # _start
+            0x00008bc0,  # bl __libc_start_main
+            0x00008d0c,  # main
+
+            0x00008d3c,  # branch not taken in main
+            0x00008d60,  # another branch, when --dostuff is passed
+            0x00008d70,  # dont do anything block
+            0x00008d7c,  # next block
+
+            0x0001d28c,  # return from strcmp
+
+            0x00008f88,  # exit from libc start main
+
+            0x00008158,  # _init
+            0x00008160,  # end of _init
+
+            0x00008b88,  # fini
+            0x00008b90,  # end of fini
+        ])
 
     def test_decree(self):
         filename = os.path.abspath(os.path.join(DIRPATH, 'binaries', 'cadet_decree_x86'))
@@ -276,17 +331,17 @@ class IntegrationTest(unittest.TestCase):
 
         self.assertEqual(len(output), 6)
 
-        self.assertIn(b'm.manticore:INFO: Verbosity set to 1.', output[0])
-        self.assertIn(b'm.manticore:INFO: Loading program ', output[1])
+        self.assertIn(b'm.c.manticore:INFO: Verbosity set to 1.', output[0])
+        self.assertIn(b'm.n.manticore:INFO: Loading program ', output[1])
 
-        self.assertIn(b'm.manticore:INFO: Generated testcase No.', output[2])
+        self.assertIn(b'm.c.manticore:INFO: Generated testcase No.', output[2])
         self.assertIn(b'- Program finished with exit status: 0', output[2])
 
-        self.assertIn(b'm.manticore:INFO: Generated testcase No.', output[3])
+        self.assertIn(b'm.c.manticore:INFO: Generated testcase No.', output[3])
         self.assertIn(b'- Program finished with exit status: 0', output[3])
 
-        self.assertIn(b'm.manticore:INFO: Results in ', output[4])
-        self.assertIn(b'm.manticore:INFO: Total time: ', output[5])
+        self.assertIn(b'm.c.manticore:INFO: Results in ', output[4])
+        self.assertIn(b'm.c.manticore:INFO: Total time: ', output[5])
 
         with open(os.path.join(workspace, "test_00000000.stdout")) as f:
             self.assertIn("Message", f.read())
@@ -301,8 +356,7 @@ class IntegrationTest(unittest.TestCase):
         #include <unistd.h>
         #include <stdint.h>
 
-        int main(int argc, char *argv[])
-        {
+        int main(int argc, char *argv[]) {
             uint8_t *p = sbrk(0);
 
             int valid_at_first = (p == sbrk(16));
@@ -312,26 +366,25 @@ class IntegrationTest(unittest.TestCase):
             sbrk(-(2<<20));
             int valid_after_bad_brk = (p == sbrk(0));
 
-            if (valid_at_first && valid_after_shift
-                && valid_after_reset && valid_after_bad_brk)
-            return 0;
+            if (valid_at_first && valid_after_shift && valid_after_reset && valid_after_bad_brk)
+                return 0;
             else
-            return 1;
+                return 1;
         }
         """
         filename = os.path.abspath(os.path.join(DIRPATH, 'binaries/brk_static_amd64'))
         workspace = f'{self.test_dir}/workspace'
+        cmd = [PYTHON_BIN, '-m', 'manticore', '--no-color', '--workspace', workspace, filename]
 
-        output = subprocess.check_output([PYTHON_BIN, '-m', 'manticore', '--no-color', '--workspace', workspace, filename])
+        output = subprocess.check_output(cmd).splitlines()
 
-        expected_output_regex = (
-            b'.*m.manticore:INFO: Loading program .+/tests/binaries/brk_static_amd64\n'
-            b'.*m.manticore:INFO: Generated testcase No. 0 - Program finished with exit status: 0\n'
-            b'.*m.manticore:INFO: Results in /tmp/[a-z0-9_]+/workspace\n'
-            b'.*m.manticore:INFO: Total time: [0-9]+.[0-9]+\n'
-        )
+        self.assertEqual(len(output), 5)
 
-        self.assertRegex(output, expected_output_regex)
+        self.assertIn(b'm.c.manticore:INFO: Verbosity set to 1.', output[0])
+        self.assertIn(b'm.n.manticore:INFO: Loading program ', output[1])
+        self.assertIn(b'm.c.manticore:INFO: Generated testcase No. 0 - Program finished with exit status: 0', output[2])
+        self.assertIn(b'm.c.manticore:INFO: Results in ', output[3])
+        self.assertIn(b'm.c.manticore:INFO: Total time: ', output[4])
 
         with open(os.path.join(workspace, "test_00000000.messages")) as f:
             self.assertIn("finished with exit status: 0", f.read())
