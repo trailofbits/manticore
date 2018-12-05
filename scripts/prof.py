558f87a39b24e7b450346d10e947376dbfac28eb
disconnect3d <dominik.b.czarnota@gmail.com>
2018-12-05 11:41:59 +0100

    Move benchmark->prof script and fix it

---
+++ scripts/prof.py
@@ -0,0 +1,37 @@
+"""
+Script used to profile Manticore runs.
+"""
+
+from __future__ import print_function
+
+from sys import argv, exit
+
+from manticore.native import Manticore
+
+
+def profile(program, sort='cumulative'):
+    print(f"[*] Profiling program \"{program}\"")
+
+    m = Manticore(program)
+    m.run(should_profile=True)
+
+    stats = m.get_profiling_stats()
+    print(f'[*] Loaded profiling data.')
+
+    if stats is None:
+        print(f"[*] Failed to collect stats for program {program}")
+        return
+
+    stats.sort_stats(sort)
+
+    stats.print_stats()
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print(f"usage: python {argv[0]} PROGRAM [SORT_METHOD]")
+        print('SORT_METHOD default is cumulative')
+        print('SORT_METHODs can be seen on https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats')
+        exit()
+
+    profile(argv[1])
