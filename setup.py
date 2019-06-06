a0cca2f09c9fcc293ffbd17c283ee85c9c7932fb
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2019-06-06 11:02:28 -0400

    Manticore 0.3.0 (#1448)
    * Manticore 0.3.0
    
    * Explicitly note lack of Constantinople support 
    
    Will hopefully head-off people with issues regarding unimplemented instructions and incorrect behavior
    
    * Add blurb about solver
    
    * Link to state machine diagram
    
    * Fix typo
    
    * Fix duplicated section
    
    And fix date

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     description="Manticore is a symbolic execution tool for analysis of binaries and smart contracts.",
     url="https://github.com/trailofbits/manticore",
     author="Trail of Bits",
-    version="0.2.5",
+    version="0.3.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     python_requires=">=3.6",
     install_requires=[
