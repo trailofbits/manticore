48366388491f82032c527eca3900791e0576271d
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2019-08-26 18:40:40 -0700

    Blacken

--- manticore/wasm/runtime_structure.py
+++ manticore/wasm/runtime_structure.py
@@ -370,7 +370,9 @@ class ModuleInstance:
             vals = []
             while not isinstance(stack.peek(), Label):
                 vals.append(stack.pop())
-                assert isinstance(vals[-1], Value.__args__), f"{type(vals[-1])} is not a value or a label"
+                assert isinstance(
+                    vals[-1], Value.__args__
+                ), f"{type(vals[-1])} is not a value or a label"
             label = stack.pop()
             assert isinstance(label, Label), f"Stack contained a {type(label)} instead of a Label"
             for v in vals[::-1]:
