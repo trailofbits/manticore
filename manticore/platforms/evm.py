800361005cfbabf70bc386e0b1f58390ea4bb280
Samuel Moelius <sam.moelius@trailofbits.com>
2019-09-06 09:58:01 -0400

    Use pc setter in _rollback.

--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -1131,7 +1131,7 @@ class EVM(Eventful):
         last_pc, last_gas, last_instruction, last_arguments, fee, allocated = self._checkpoint_data
         self._push_arguments(last_arguments)
         self._gas = last_gas
-        self._pc = last_pc
+        self.pc = last_pc
         self._allocated = allocated
         self._checkpoint_data = None
 
