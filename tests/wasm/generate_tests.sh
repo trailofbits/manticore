12908e654600125247ab4ec6bcf909dd8ba6d9cc
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2019-09-26 16:12:52 -0700

    Download updated WABT
    It's fixed now!

--- tests/wasm/generate_tests.sh
+++ tests/wasm/generate_tests.sh
@@ -3,22 +3,14 @@
 rm -rf */
 touch __init__.py
 
-# The WABT release is outdated so we can't just download it as of Sep 2019
-git clone --recursive https://github.com/WebAssembly/wabt
-cd wabt
-make
-cp bin/wast2json ..
-cd ..
-rm -rf wabt
-
-wget -nc -nv -O wabt.tgz -c https://github.com/WebAssembly/wabt/releases/download/1.0.11/wabt-1.0.11-linux.tar.gz
+wget -nc -nv -O wabt.tgz -c https://github.com/WebAssembly/wabt/releases/download/1.0.12/wabt-1.0.12-linux.tar.gz
 wget -nc -nv -O spec.zip -c https://github.com/WebAssembly/spec/archive/ace189a8f906f4f0760656a2aa53b6dc7046e771.zip
 
 yes | unzip -q -j spec.zip 'spec-*/test/core/*' -d .
 rm run.py README.md 
 rm spec.zip
 
-#tar --wildcards --strip=1 -xf wabt.tgz 'wabt-*/wast2json'
+tar --wildcards --strip=1 -xf wabt.tgz 'wabt-*/wast2json'
 rm wabt.tgz
 
 mkdir skipped_tests
