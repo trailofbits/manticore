254c6c85a3e908ec3710fecec20fa9e2a597aa80
Samuel Moelius <sam.moelius@trailofbits.com>
2020-01-06 20:34:59 +0000

    Limit paths that typeguard checks.

--- manticore/ethereum/manticore.py
+++ manticore/ethereum/manticore.py
@@ -1,6 +1,7 @@
 from typeguard.importhook import install_import_hook
 
-install_import_hook("manticore")
+install_import_hook("manticore.platforms.evm")
+install_import_hook("manticore.platforms.evm_world_state")
 
 import copy
 import itertools
