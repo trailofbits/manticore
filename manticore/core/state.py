ce5d4b2099407b32b72eb14787f8222aecff86dc
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-01-24 10:25:46 -0800

    Add test and blacken

--- manticore/core/state.py
+++ manticore/core/state.py
@@ -58,6 +58,7 @@ class SerializeState(Concretize):
     """ Allows the user to save a copy of the current state somewhere on the
         disk so that analysis can later be resumed from this point.
     """
+
     def setstate(self, state, _value):
         from ..utils.helpers import PickleSerializer
 
