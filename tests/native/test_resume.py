318a9573da4ab1623c3261e6a16ddc2857e6fd37
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-01-24 14:11:01 -0800

    Rewrite native test
    No longer relies on instruction counts, instead checking that the numbers of states are correct and that the correct output is produced.

--- tests/native/test_resume.py
+++ tests/native/test_resume.py
@@ -3,14 +3,9 @@ from manticore.native import Manticore
 from manticore.core.plugin import Plugin
 from manticore.core.state import SerializeState, TerminateState
 from pathlib import Path
+import os
 
-
-class InsnCounterPlugin(Plugin):
-    def did_execute_instruction_callback(self, state, last_pc, pc, instruction):
-        with self.locked_context("counter", dict) as ctx:
-            val = ctx.setdefault(instruction.mnemonic, 0)
-            ctx[instruction.mnemonic] = val + 1
-
+from manticore.utils.log import set_verbosity
 
 ms_file = str(
     Path(__file__).parent.parent.parent.joinpath("examples", "linux", "binaries", "multiple-styles")
@@ -19,43 +14,34 @@ ms_file = str(
 
 class TestResume(unittest.TestCase):
     def test_resume(self):
-        m = Manticore(ms_file, stdin_size=18)
-        plugin = InsnCounterPlugin()
-        m.register_plugin(plugin)
-        m.run()
+        m = Manticore(ms_file, stdin_size=17)
 
-        counts_canonical = plugin.context.get("counter")
-
-        m = Manticore(ms_file, stdin_size=18)
-        plugin = InsnCounterPlugin()
-        m.register_plugin(plugin)
-
-        @m.hook(0x0400A55)
+        # First instruction of `main`
+        @m.hook(0x4009AE)
         def serialize(state):
-            with m.locked_context() as context:
-                if context.get("kill", False):
-                    raise TerminateState("Abandoning...")
-                context["kill"] = True
+            if os.path.exists("/tmp/ms_checkpoint.pkl"):
+                raise TerminateState("Abandoning...")
             raise SerializeState("/tmp/ms_checkpoint.pkl")
 
         m.run()
-
-        counts_save = plugin.context.get("counter")
+        self.assertEqual(m.count_terminated_states(), 1)
+        for state in m.terminated_states:
+            self.assertEqual(state.cpu.PC, 0x4009AE)
 
         m = Manticore.from_saved_state("/tmp/ms_checkpoint.pkl")
-        plugin = InsnCounterPlugin()
-        m.register_plugin(plugin)
+        self.assertEqual(m.count_ready_states(), 1)
+        for st in m.ready_states:
+            self.assertEqual(state.cpu.PC, 0x4009AE)
         m.run()
 
-        counts_resume = plugin.context.get("counter")
-
-        for k in counts_canonical:
-            with self.subTest(k):
-                self.assertEqual(
-                    counts_save.get(k, 0) + counts_resume.get(k, 0),
-                    counts_canonical[k],
-                    f"Mismatched {k} count",
-                )
+        self.assertEqual(m.count_terminated_states(), 18)
+        self.assertTrue(
+            any("exit status: 0" in str(st._terminated_by) for st in m.terminated_states)
+        )
+        m.finalize()
+        for st in m.terminated_states:
+            if "exit status: 0" in str(st._terminated_by):
+                self.assertEqual(st.solve_one(st.input_symbols[0]), b"coldlikeminisodas")
 
 
 if __name__ == "__main__":
