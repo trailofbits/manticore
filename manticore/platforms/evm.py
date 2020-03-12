55c51cce2cdb67cc66e47944b3f5a00c38803eae
feliam <felipe.andres.manzano@gmail.com>
2020-03-12 15:51:21 -0300

    fix pyevmasm import

--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -32,7 +32,7 @@ from ..utils.event import Eventful
 from ..utils import config
 from ..core.smtlib.visitors import simplify
 from ..exceptions import EthereumError
-import manticore.ethereum.evmasm as EVMAsm
+from ..ethereum import evmasm as EVMAsm
 import logging
 from collections import namedtuple
 import sha3
