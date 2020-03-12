c4eeb93f857ff1320a0b837523dd12cabe81b5aa
feliam <felipe.andres.manzano@gmail.com>
2020-03-12 13:49:00 -0300

    blkn

--- tests/ethereum/test_EVMAssembler.py
+++ tests/ethereum/test_EVMAssembler.py
@@ -6,7 +6,7 @@ import pyevmasm as EVMAsm
 
 def int_to_bytes(i):
     if sys.version_info[0] >= 3:
-        return i.to_bytes(1, 'little')
+        return i.to_bytes(1, "little")
     else:
         return bytes(chr(i))
 
@@ -19,12 +19,14 @@ class EVMTest_Assembler(unittest.TestCase):
     def test_ADD_1(self):
         instruction = EVMAsm.disassemble_one(b"\x60\x10")
         self.assertEqual(
-            EVMAsm.Instruction(0x60, "PUSH", 1, 0, 1, 3, "Place 1 byte item on stack.", 16, 0), instruction
+            EVMAsm.Instruction(0x60, "PUSH", 1, 0, 1, 3, "Place 1 byte item on stack.", 16, 0),
+            instruction,
         )
 
         instruction = EVMAsm.assemble_one("PUSH1 0x10")
         self.assertEqual(
-            instruction, EVMAsm.Instruction(0x60, "PUSH", 1, 0, 1, 3, "Place 1 byte item on stack.", 16, 0)
+            instruction,
+            EVMAsm.Instruction(0x60, "PUSH", 1, 0, 1, 3, "Place 1 byte item on stack.", 16, 0),
         )
 
         instructions1 = EVMAsm.disassemble_all(b"\x30\x31")
@@ -95,42 +97,40 @@ class EVMTest_Assembler(unittest.TestCase):
         self.assertTrue(insn.mnemonic == "CREATE2")
 
     def test_istanbul_fork(self):
-        insn = EVMAsm.disassemble_one(b'\x31', fork='istanbul')
-        self.assertTrue(insn.mnemonic == 'BALANCE')
+        insn = EVMAsm.disassemble_one(b"\x31", fork="istanbul")
+        self.assertTrue(insn.mnemonic == "BALANCE")
         self.assertTrue(insn.fee == 700)
         self.assertTrue(insn.pops == 1)
         self.assertTrue(insn.pushes == 1)
-        insn = EVMAsm.disassemble_one(b'\x3f', fork='istanbul')
-        self.assertTrue(insn.mnemonic == 'EXTCODEHASH')
+        insn = EVMAsm.disassemble_one(b"\x3f", fork="istanbul")
+        self.assertTrue(insn.mnemonic == "EXTCODEHASH")
         self.assertTrue(insn.fee == 700)
         self.assertTrue(insn.pops == 1)
         self.assertTrue(insn.pushes == 1)
-        insn = EVMAsm.disassemble_one(b'\x46', fork='istanbul')
-        self.assertTrue(insn.mnemonic == 'CHAINID')
+        insn = EVMAsm.disassemble_one(b"\x46", fork="istanbul")
+        self.assertTrue(insn.mnemonic == "CHAINID")
         self.assertTrue(insn.fee == 2)
         self.assertTrue(insn.pops == 0)
         self.assertTrue(insn.pushes == 1)
-        insn = EVMAsm.disassemble_one(b'\x47', fork='istanbul')
-        self.assertTrue(insn.mnemonic == 'SELFBALANCE')
+        insn = EVMAsm.disassemble_one(b"\x47", fork="istanbul")
+        self.assertTrue(insn.mnemonic == "SELFBALANCE")
         self.assertTrue(insn.fee == 5)
         self.assertTrue(insn.pops == 0)
         self.assertTrue(insn.pushes == 1)
-        insn = EVMAsm.disassemble_one(b'\x54', fork='istanbul')
-        self.assertTrue(insn.mnemonic == 'SLOAD')
+        insn = EVMAsm.disassemble_one(b"\x54", fork="istanbul")
+        self.assertTrue(insn.mnemonic == "SLOAD")
         self.assertTrue(insn.fee == 800)
         self.assertTrue(insn.pops == 1)
         self.assertTrue(insn.pushes == 1)
-
 
     def test_assemble_DUP1_regression(self):
         insn = EVMAsm.assemble_one("DUP1")
         self.assertEqual(insn.mnemonic, "DUP1")
         self.assertEqual(insn.opcode, 0x80)
 
-
     def test_assemble_LOGX_regression(self):
         inst_table = EVMAsm.instruction_tables[EVMAsm.DEFAULT_FORK]
-        log0_opcode = 0xa0
+        log0_opcode = 0xA0
         for n in range(5):
             opcode = log0_opcode + n
             self.assertTrue(opcode in inst_table, "{!r} not in instruction_table".format(opcode))
@@ -139,7 +139,6 @@ class EVMTest_Assembler(unittest.TestCase):
             insn = EVMAsm.assemble_one(asm)
             self.assertEqual(insn.mnemonic, asm)
             self.assertEqual(insn.opcode, opcode)
-
 
     def test_consistency_assembler_disassembler(self):
         """
@@ -155,5 +154,5 @@ class EVMTest_Assembler(unittest.TestCase):
             self.assertEqual(inst_dis, inst_as)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
