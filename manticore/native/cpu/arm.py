42193fd65eeba4fd84dba3d9a4c08c63828bb1cd
Brad Larsen <brad.larsen@trailofbits.com>
2020-03-20 20:20:22 -0400

    Fix ARMv7 EOR implementation for modified immediates

--- manticore/native/cpu/arm.py
+++ manticore/native/cpu/arm.py
@@ -1507,7 +1507,7 @@ class Armv7Cpu(Cpu):
         else:
             cpu._bitwise_instruction(lambda x, y: x | ~y, dest, dest, op1)
 
-    @instruction
+    @instruction(can_take_denormalized_mod_imm=True)
     def EOR(cpu, dest, op1, op2=None):
         if op2 is not None:
             cpu._bitwise_instruction(lambda x, y: x ^ y, dest, op1, op2)
