f933e585cdf93af948db3aa98ba5b3ec2b6efd04
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-05-11 10:14:27 -0400

    Rollback in parent state instead of children states

--- manticore/native/state.py
+++ manticore/native/state.py
@@ -69,22 +69,22 @@ class State(StateBase):
             # Need to define local variable to use in closure
             e = exc
             expression = self.cpu.read_register(e.reg_name)
+            if e.rollback:
+                self.rollback(self._checkpoint_data)
 
             def setstate(state: State, value):
                 state.cpu.write_register(e.reg_name, value)
-                if e.rollback:
-                    state.rollback(self._checkpoint_data)
 
             raise Concretize(str(e), expression=expression, setstate=setstate, policy=e.policy)
         except ConcretizeMemory as exc:
             # Need to define local variable to use in closure
             e = exc
             expression = self.cpu.read_int(e.address, e.size)
+            if e.rollback:
+                self.rollback(self._checkpoint_data)
 
             def setstate(state: State, value):
                 state.cpu.write_int(e.address, value, e.size)
-                if e.rollback:
-                    state.rollback(self._checkpoint_data)
 
             raise Concretize(str(e), expression=expression, setstate=setstate, policy=e.policy)
         except MemoryException as e:
