bc1edd56a7e7595976eb3c7bf58356f3d4226fbf
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-05-14 17:40:51 -0400

    Remove outdated function name in comment

--- tests/native/test_state.py
+++ tests/native/test_state.py
@@ -29,7 +29,7 @@ class FakeCpu:
     def __init__(self):
         self._memory = FakeMemory()
 
-        # Required for mocking of `State._checkpoint()` and `State.rollback()`
+        # Required for mocking of checkpoint and `State._rollback()`
         self.PC = 0
         self._last_pc = None
 
