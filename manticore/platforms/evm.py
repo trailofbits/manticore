a7ca2f5e38f7f5958fe11b90876a3ac94d51f58d
feliam <felipe.andres.manzano@gmail.com>
2020-05-20 16:03:56 -0300

    Update manticore/platforms/evm.py
    Co-authored-by: Eric Hennenfent <eric.hennenfent@trailofbits.com>

--- manticore/platforms/evm.py
+++ manticore/platforms/evm.py
@@ -85,7 +85,7 @@ consts.add(
     "txfail",
     default="optimistic",
     description=(
-        "Default behavior for tansaction failing because not enough funds."
+        "Default behavior for transaction failing because not enough funds."
         "optimistic: Assume there is always enough funds to pay for value and gas. Wont fork"
         "pesimistic: Assume the balance is never enough for paying fo a transaction. Wont fork"
         "both: Will fork for both options if possible."
