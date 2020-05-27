f912266fc8995c62414dde25cc7654a87733fed3
feliam <felipe.andres.manzano@gmail.com>
2020-05-27 11:26:09 -0300

    initial import manticore verifier

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
     ]
     + rtd_dependent_deps(),
     extras_require=extra_require,
-    entry_points={"console_scripts": ["manticore = manticore.__main__:main"]},
+    entry_points={"console_scripts": ["manticore = manticore.__main__:main",
+                                      "manticore_verifier = manticore.ethereum.verifier.__main__:main"]},
     classifiers=["License :: OSI Approved :: GNU Affero General Public License v3"],
 )
