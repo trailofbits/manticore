6747143eca0de962c6fcca0c41bd5aff944b479f
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-05-28 12:54:31 -0700

    Fix Black (#1718)
    * Use merge SHA instead of head
    
    Github Actions doesn't update the head variable despite the checkout action merging the active branch into `master`, so the commit range specified was invalid
    
    * Start from common ancestor
    
    * Omit explicit SHA
    
    * Lint HEAD instead of merge
    
    * Manually fetch base SHA
    
    * Print the names of files to be diffed
    
    * Update ci.yml
    
    * Modify a Python file
    
    * Blacken everything that was missing
    
    The modified files got missed due to black not properly running on our CI

--- scripts/pyfile_exists.py
+++ scripts/pyfile_exists.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+# Checks whether files listed via stdin actually exist
 for f in sys.stdin.readlines():
     line = f.strip()
     if line.endswith(".py") and os.path.exists(line):
