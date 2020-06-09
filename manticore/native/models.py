bdbd0391c876450ad0a4353e9c79f695e3f5c5c5
sschriner <60201678+sschriner@users.noreply.github.com>
2020-06-09 17:12:19 -0400

    Symbolic model for strcpy (#1681)
    * Working copy for strcpy - without symbolic input
    
    * Corrected null term error
    
    * Needs to be tested but general algorithm idea for handling symbolic input
    
    * Updated according to feedback
    
    * Beginning of strcpy tests
    
    * Revising strcpy algorithm - realised it was flawed
    
    * This should be a more correct way of handling symbolic bytes. Needs testing
    
    * Get rid of redundant code
    
    * Fixed another typo
    
    * Update manticore/native/models.py
    
    Co-Authored-By: Eric Kilmer <eric.d.kilmer@gmail.com>
    
    * Clarified and updated comments
    
    * Fixed some errors in model while working on tests
    
    * Progress on tests
    
    * Found typo in models
    
    * Removed unnecassary whitespace
    
    * Added some helper functions for tests and to clean things up
    
    * Added comments for helper functions
    
    * WIP: current state of strcpy tests
    
    * Updated type hints from Expression to BitVec
    
    * Fixed linter complaints
    
    * Updated strcpy return type to make linter happy
    
    * Added some helper properties to expression BitVecITE
    
    * Overwrote equal op in ArraySelect
    
    * Fixed formatting in expression
    
    * Almost done just needs some cleaning
    
    * Attempt at overriding __eq__ again
    
    * Cleaned up tests some still have unresolved __eq__ issues
    
    * Removed stuff from expression and updated test_models.py
    
    * Removed stuff from expression and updated test_models.py
    
    * Basicly done probably needs some more comments for clarity
    
    * Cleaned up the tests a little
    
    * Corrected linter error and added more comments to strcpy model functions
    
    * Remove duplicated word in comment
    
    Co-authored-by: Eric Kilmer <eric.d.kilmer@gmail.com>
    
    * Update manticore/native/models.py
    
    Co-authored-by: Brad Larsen <brad.larsen@trailofbits.com>
    
    * Update manticore/native/models.py
    
    Co-authored-by: Brad Larsen <brad.larsen@trailofbits.com>
    
    * Update tests/native/test_models.py
    
    Co-authored-by: Brad Larsen <brad.larsen@trailofbits.com>
    
    * Updates according to feedback
    
    * Swapped can_be_true and must be true
    
    * Updates for feedback
    
    * Forgot to lint
    
    * Fixed some typos
    
    * Largely simplified ITE being built
    
    * Forgot to lint
    
    * This is messy but seems to be concretizing to the correct states
    
    * Very very messy state of the code before attempting to concretize on every possible NULL
    
    * Cleaned up model with forking on every possible NULL
    
    * Removed trailing whitespace for CodeClimate
    
    * Added state.context for offset value back
    
    * New symbolic test with regex for expected output
    
    * New symbolic test with regex for expected output
    
    * Updated for mypy
    
    * Updated comments in tests
    
    * Updated cant_be_null to cannot_be_null
    
    * Corrected docstring location for _test_strcpy
    
    * Add break for efficiency
    
    * Added compiler information and source file
    
    * Updated regex to use raw strings
    
    Co-authored-by: Eric Kilmer <eric.d.kilmer@gmail.com>
    Co-authored-by: Brad Larsen <brad.larsen@trailofbits.com>

--- manticore/native/models.py
+++ manticore/native/models.py
@@ -2,10 +2,13 @@
 Models here are intended to be passed to :meth:`~manticore.native.state.State.invoke_model`, not invoked directly.
 """
 
-from .cpu.abstractcpu import ConcretizeArgument
-from ..core.smtlib import issymbolic
+from .cpu.abstractcpu import Cpu, ConcretizeArgument
+from .state import State
+from ..core.smtlib import issymbolic, BitVec
 from ..core.smtlib.solver import SelectedSolver
 from ..core.smtlib.operators import ITEBV, ZEXTEND
+from ..core.state import Concretize
+from typing import Union
 
 VARIADIC_FUNC_ATTR = "_variadic"
 
@@ -57,7 +60,7 @@ def _find_zero(cpu, constrs, ptr):
     return offset
 
 
-def strcmp(state, s1, s2):
+def strcmp(state: State, s1: Union[int, BitVec], s2: Union[int, BitVec]):
     """
     strcmp symbolic model.
 
@@ -76,8 +79,8 @@ def strcmp(state, s1, s2):
     the two
 
     :param State state: Current program state
-    :param int s1: Address of string 1
-    :param int s2: Address of string 2
+    :param s1: Address of string 1
+    :param s2: Address of string 2
     :return: Symbolic strcmp result
     :rtype: Expression or int
     """
@@ -113,14 +116,14 @@ def strcmp(state, s1, s2):
     return ret
 
 
-def strlen(state, s):
+def strlen(state: State, s: Union[int, BitVec]) -> Union[int, BitVec]:
     """
     strlen symbolic model.
 
     Algorithm: Walks from end of string not including NULL building ITE tree when current byte is symbolic.
 
     :param State state: current program state
-    :param int s: Address of string
+    :param s: Address of string
     :return: Symbolic strlen result
     :rtype: Expression or int
     """
@@ -139,4 +142,97 @@ def strlen(state, s):
         if issymbolic(byt):
             ret = ITEBV(cpu.address_bit_size, byt == 0, offset, ret)
 
+    return ret
+
+
+def is_definitely_NULL(byte, constrs) -> bool:
+    """
+    Checks if a given byte read from memory is NULL.
+    This supports both concrete & symbolic byte values.
+
+    :param byte: byte read from memory to be examined
+    :param constrs: state constraints
+    :return: whether a given byte is NULL or constrained to NULL
+    """
+    if issymbolic(byte):
+        return SelectedSolver.instance().must_be_true(constrs, byte == 0)
+    else:
+        return byte == 0
+
+
+def cannot_be_NULL(byte, constrs) -> bool:
+    """
+    Checks if a given byte read from memory is not NULL or cannot be NULL
+
+    :param byte: byte read from memory to be examined
+    :param constrs: state constraints
+    :return: whether a given byte is not NULL or cannot be NULL
+    """
+    if issymbolic(byte):
+        return SelectedSolver.instance().must_be_true(constrs, byte != 0)
+    else:
+        return byte != 0
+
+
+def can_be_NULL(byte, constrs) -> bool:
+    """
+    Checks if a given byte read from memory can be NULL
+
+    :param byte: byte read from memory to be examined
+    :param constrs: state constraints
+    :return: whether a given byte is NULL or can be NULL
+    """
+    if issymbolic(byte):
+        return SelectedSolver.instance().can_be_true(constrs, byte == 0)
+    else:
+        return byte == 0
+
+
+def strcpy(state: State, dst: Union[int, BitVec], src: Union[int, BitVec]) -> Union[int, BitVec]:
+    """
+    strcpy symbolic model
+
+    Algorithm: Copy every byte from src to dst until finding a byte that is NULL or is
+    constrained to only the NULL value. Every time a byte is fouund that can be NULL but
+    is not definetly NULL concretize and fork states.
+
+    :param state: current program state
+    :param dst: destination string address
+    :param src: source string address
+    :return: pointer to the dst
+    """
+    if issymbolic(src):
+        raise ConcretizeArgument(state.cpu, 1)
+
+    if issymbolic(dst):
+        raise ConcretizeArgument(state.cpu, 0)
+
+    cpu = state.cpu
+    constrs = state.constraints
+    ret = dst
+
+    # Initialize offset based on whether state has been forked in strcpy
+    if "strcpy" not in state.context:
+        offset = 0
+    else:
+        offset = state.context["strcpy"]
+
+    # Copy until a src_byte is symbolic and constrained to '\000', or is concrete and '\000'
+    src_val = cpu.read_int(src + offset, 8)
+    while not is_definitely_NULL(src_val, constrs):
+        cpu.write_int(dst + offset, src_val, 8)
+
+        # If a byte can be NULL set the src_val for concretize and fork states
+        if can_be_NULL(src_val, constrs):
+            state.context["strcpy"] = offset
+            raise Concretize("Forking on NULL strcpy", expression=(src_val == 0), policy="ALL")
+        offset += 1
+
+        src_val = cpu.read_int(src + offset, 8)
+
+    # Write concrete null for end of string in current state
+    cpu.write_int(dst + offset, 0, 8)
+
+    if "strcpy" in state.context:
+        del state.context["strcpy"]
     return ret
