19f4d435db6600c59819338db1b4873b23215e54
feliam <felipe.andres.manzano@gmail.com>
2020-08-17 14:37:48 -0300

    BitVec Bitvec

--- examples/script/concolic.py
+++ examples/script/concolic.py
@@ -66,18 +66,18 @@ def flip(constraint):
     """
     flips a constraint (Equal)
 
-    (Equal (BitVecITE Cond IfC ElseC) IfC)
+    (Equal (BitvecITE Cond IfC ElseC) IfC)
         ->
-    (Equal (BitVecITE Cond IfC ElseC) ElseC)
+    (Equal (BitvecITE Cond IfC ElseC) ElseC)
     """
     equal = copy.copy(constraint)
 
     assert len(equal.operands) == 2
     # assume they are the equal -> ite form that we produce on standard branches
     ite, forcepc = equal.operands
-    if not (isinstance(ite, BitVecITE) and isinstance(forcepc, BitvecConstant)):
+    if not (isinstance(ite, BitvecITE) and isinstance(forcepc, BitvecConstant)):
         return constraint
-    assert isinstance(ite, BitVecITE) and isinstance(forcepc, BitvecConstant)
+    assert isinstance(ite, BitvecITE) and isinstance(forcepc, BitvecConstant)
     assert len(ite.operands) == 3
     cond, iifpc, eelsepc = ite.operands
     assert isinstance(iifpc, BitvecConstant) and isinstance(eelsepc, BitvecConstant)
