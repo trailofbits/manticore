4eb634b6f022ff633ddbd8b72e3539ead8e766a0
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-08-28 13:06:12 -0700

    Drop max verbosity in logging tests
    Haven't been able to figure out why, but somehow other loggers get "stuck" at this high verbosity and the integration tests try to print out the values of every single register.

--- tests/native/test_logging.py
+++ tests/native/test_logging.py
@@ -10,10 +10,6 @@ class ManticoreLogger(unittest.TestCase):
     _multiprocess_can_split_ = True
 
     def test_logging(self):
-        set_verbosity(5)
-        self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.DEBUG)
-        self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.DEBUG)
-
         set_verbosity(1)
         self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.WARNING)
         self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.INFO)
