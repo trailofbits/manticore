ec0c595a58c9a62d0e5e8e3132e79dc4f5f46265
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-08-27 19:37:16 -0700

    Split up base Manticore tests and logging tests
    The verbosity changes seem to be taking hold when they shouldn't

---
+++ tests/native/test_logging.py
@@ -0,0 +1,21 @@
+import unittest
+import logging
+
+from manticore.utils.log import get_verbosity, set_verbosity
+
+
+class ManticoreLogger(unittest.TestCase):
+    """Make sure we set the logging levels correctly"""
+
+    _multiprocess_can_split_ = True
+
+    def test_logging(self):
+        set_verbosity(5)
+        self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.DEBUG)
+        self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.DEBUG)
+
+        set_verbosity(1)
+        self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.WARNING)
+        self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.INFO)
+
+        set_verbosity(0)
--- tests/native/test_manticore.py
+++ tests/native/test_manticore.py
@@ -134,20 +134,3 @@ class ManticoreTest(unittest.TestCase):
         else:
             self.assertTrue(a <= 0x41)
             self.assertTrue(b > 0x41)
-
-
-class ManticoreLogger(unittest.TestCase):
-    """Make sure we set the logging levels correctly"""
-
-    _multiprocess_can_split_ = True
-
-    def test_logging(self):
-        set_verbosity(5)
-        self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.DEBUG)
-        self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.DEBUG)
-
-        set_verbosity(1)
-        self.assertEqual(get_verbosity("manticore.native.cpu.abstractcpu"), logging.WARNING)
-        self.assertEqual(get_verbosity("manticore.ethereum.abi"), logging.INFO)
-
-        set_verbosity(0)
