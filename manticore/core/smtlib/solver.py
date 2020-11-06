0f62e651d510295e4d18b0b46de738d5abbff51f
Eric Hennenfent <eric.hennenfent@trailofbits.com>
2020-11-05 17:09:14 -0800

    Double default solver timeout to 240 seconds

--- manticore/core/smtlib/solver.py
+++ manticore/core/smtlib/solver.py
@@ -44,7 +44,7 @@ class SolverType(config.ConfigEnum):
 
 logger = logging.getLogger(__name__)
 consts = config.get_group("smt")
-consts.add("timeout", default=120, description="Timeout, in seconds, for each Z3 invocation")
+consts.add("timeout", default=240, description="Timeout, in seconds, for each Z3 invocation")
 consts.add("memory", default=1024 * 8, description="Max memory for Z3 to use (in Megabytes)")
 consts.add(
     "maxsolutions",
