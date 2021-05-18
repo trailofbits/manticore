fbf8a507b17c96c766c0e4a16556981b80274810
Eric Kilmer <eric.d.kilmer@gmail.com>
2021-05-18 11:39:42 -0400

    Encode SymbolicFile wildcard before checking length

--- manticore/platforms/linux.py
+++ manticore/platforms/linux.py
@@ -475,8 +475,10 @@ class SymbolicFile(File):
         super().__init__(path, flags)
 
         # Convert to numeric value because we read the file as bytes
-        assert len(wildcard) == 1
-        wildcard_val = ord(wildcard)
+        wildcard_val = wildcard.encode()
+        assert (
+            len(wildcard_val) == 1
+        ), f"SymbolicFile wildcard needs to be a single byte, not {wildcard_val!r}"
 
         # read the concrete data using the parent the read() form the File class
         data = self.file.read()
